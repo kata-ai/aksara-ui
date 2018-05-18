1977600d4149cc24f65023a30185c7bcc76e1e63
Reyhan Sofian <computecoholic@gmail.com>
2018-05-18 22:09:47 +0700

    [FIX] Loader Asset and Icon (#7)
    * fix robot loader and kata icons
    
    * remove typing on package.json
    
    * remove comment
    
    * remove comment on tsconfig

--- src/components/Loading/Robot.tsx
+++ src/components/Loading/Robot.tsx
@@ -1,8 +1,7 @@
 import React, { SFC } from 'react';
 
+import Loader from '../../assets/images/loader.svg';
 import './Robot.scss';
-
-const Loader = require('assets/images/loader.svg');
 
 interface Props {}
 
