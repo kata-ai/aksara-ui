aab96513217ef8bc2d9e8812325047c9d0ef1566
Resi Respati <resir014@gmail.com>
2018-08-04 00:54:42 +0700

    feat(dropdown): added initial dropdown component

---
+++ packages/dropdown/README.md
@@ -0,0 +1,20 @@
+# @kata-kit/dropdown
+
+> Dropdown component for kata-kit.
+
+## Install
+
+```sh
+# yarn
+yarn add @kata-kit/dropdown
+# npm
+npm install @kata-kit/dropdown
+```
+
+## Usage
+
+To use this component within your React app, import as follows:
+
+```javascript
+import { Dropdown } from '@kata-kit/dropdown';
+```
---
+++ packages/dropdown/src/index.ts
@@ -0,0 +1,13 @@
+import Dropdown from './Dropdown';
+import DropdownSelector from './DropdownSelector';
+import DropdownItem from './DropdownItem';
+import DropdownMenu from './DropdownMenu';
+import DropdownToggle from './DropdownToggle';
+
+export {
+  Dropdown,
+  DropdownSelector,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle
+};
