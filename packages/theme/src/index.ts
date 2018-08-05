7ec3aded9c3adaa35f0c82e7dcb6e1a437533f22
Resi Respati <resir014@gmail.com>
2018-08-05 16:33:12 +0700

    refactor(theme): add ThemeAttributes property for context preparation

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -2,8 +2,13 @@ import defaultTheme from './defaultTheme';
 import KataKitThemeProvider, {
   KataKitThemeProviderProps
 } from './KataKitThemeProvider';
-import { KataKitDefaultTheme } from './types';
+import { KataKitDefaultTheme, ThemeAttributes } from './types';
 
 // TODO: switch away from ThemeProvider to React Context API
 export default KataKitThemeProvider;
-export { defaultTheme, KataKitDefaultTheme, KataKitThemeProviderProps };
+export {
+  defaultTheme,
+  KataKitDefaultTheme,
+  KataKitThemeProviderProps,
+  ThemeAttributes
+};
--- packages/theme/src/types.ts
+++ packages/theme/src/types.ts
@@ -2,6 +2,11 @@ import defaultTheme from './defaultTheme';
 
 export type KataKitDefaultTheme = typeof defaultTheme;
 
+export type ThemeValue = string;
+export type ThemeModeAttributes = { [mode: string]: ThemeValue };
+export type ThemeModeProps = { mode: string };
+export type ThemeProps = { theme: ThemeModeProps };
+
 export type ThemeAttributes = {
   backgroundColor: string;
   textColor: string;
