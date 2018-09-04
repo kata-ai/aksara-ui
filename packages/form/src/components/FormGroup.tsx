0598baced65918fa6d3e8bb72e9f56126a7e680a
Resi Respati <resir014@gmail.com>
2018-09-04 17:29:30 +0700

    feat(form): add FormGroup

---
+++ packages/form/src/components/FormGroup.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { variables } from '@kata-kit/theme';
+
+interface FormGroupProps {
+  className?: string;
+}
+
+const FormGroup: React.SFC<FormGroupProps> = ({ className, children }) => (
+  <Div className={className}>{children}</Div>
+);
+
+export default FormGroup;
+
+const Div = styled('div')`
+  margin-bottom: ${variables.spaces.space2};
+`;
