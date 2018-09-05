db86a5fa182514185ba0badc3db59f05a73a6c44
Resi Respati <resir014@gmail.com>
2018-09-05 19:32:22 +0700

    docs(badge): added badge docs + examples

---
+++ packages/badge/examples/0-basic-example.raw.js
@@ -0,0 +1,8 @@
+() => (
+  <Fragment>
+    <Badge>entity</Badge> <Badge color="primary">primary</Badge>{' '}
+    <Badge color="secondary">secondary</Badge>{' '}
+    <Badge color="success">success</Badge>{' '}
+    <Badge color="warning">warning</Badge> <Badge color="danger">danger</Badge>{' '}
+  </Fragment>
+);
