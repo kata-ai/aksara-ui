9c99987652fc5ebecd97385011c8e299facbf1cd
Resi Respati <resir014@gmail.com>
2018-09-05 12:58:15 +0700

    docs(form): added prop documentation for props

---
+++ packages/form/docs/props.ts
@@ -0,0 +1,22 @@
+import { PropAttributesMap } from '@kata-kit/doc-utils';
+
+export const inputProps: PropAttributesMap = {
+  errors: {
+    type: 'boolean',
+    description: 'Alternate form state for input with errors.',
+    defaultValue: 'false'
+  },
+  className: {
+    type: 'string',
+    description: 'Additional CSS classes to give to the button.',
+    defaultValue: 'undefined'
+  }
+};
+
+export const formProps: PropAttributesMap = {
+  className: {
+    type: 'string',
+    description: 'Additional CSS classes to give to the button.',
+    defaultValue: 'undefined'
+  }
+};
