40d6c7dcbe84d5e1c8294ef297171ff329cb76a5
Reyhan Sofian Haqqi <computecoholic@gmail.com>
2018-09-11 11:33:19 +0700

    chore(docs): update button docs

--- packages/button/docs/props.ts
+++ packages/button/docs/props.ts
@@ -64,3 +64,62 @@ export const buttonProps: PropAttributesMap = {
     defaultValue: 'false'
   }
 };
+
+export const floatingButtonProps: PropAttributesMap = {
+  disabled: {
+    type: 'boolean',
+    description: 'Whether the button is disabled or not.',
+    defaultValue: 'false'
+  },
+  color: {
+    type: 'ButtonColors',
+    oneOf: [
+      "'primary'",
+      "'secondary'",
+      "'success'",
+      "'danger'",
+      "'warning'",
+      "'info'",
+      "'white'"
+    ],
+    description: 'The color of the button.',
+    defaultValue: "'secondary'"
+  },
+  icon: {
+    type: 'string',
+    description: 'Icon of the button',
+    defaultValue: 'undefined'
+  },
+  size: {
+    type: 'ButtonSizes',
+    oneOf: ["'sm'", "'lg'"],
+    description: 'The size of the button.',
+    defaultValue: "'lg'"
+  },
+  className: {
+    type: 'string',
+    description: 'Additional CSS classes to give to the button.',
+    defaultValue: 'undefined'
+  },
+  onClick: {
+    type: 'React.MouseEventHandler<HTMLButtonElement>',
+    description: 'Event handler triggered during <code>onClick</code>.',
+    defaultValue: 'undefined'
+  },
+  type: {
+    type: 'string',
+    description: 'The default HTML type of the button.',
+    defaultValue: "'button'"
+  },
+  active: {
+    type: 'boolean',
+    deprecated: true,
+    description: 'Whether the button is active or not.',
+    defaultValue: 'false'
+  },
+  isLoading: {
+    type: 'boolean',
+    description: 'Triggers the loading state of the button.',
+    defaultValue: 'false'
+  }
+};
---
+++ packages/button/examples/1-floating-example.raw.js
@@ -0,0 +1,8 @@
+() => (
+  <Fragment>
+    <div style={{ marginBottom: '8px' }}>
+      <FloatingButton icon="add" /> <FloatingButton icon="tick" />{' '}
+      <FloatingButton icon="add" isLoading />{' '}
+    </div>
+  </Fragment>
+);
