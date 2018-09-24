879e3051ee4df8293cc255662133353ef82ee0a9
Resi Respati <resir014@gmail.com>
2018-09-24 16:37:39 +0700

    chore: initial step of ts 3.0 + project references

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
+import path from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
-  input: './tsc-out/index.js',
   output: [
     {
       file: './lib/index.js',
