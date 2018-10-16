8f4c48ea063a41979060612329a1f0d2c9b0a30b
Resi Respati <resir014@gmail.com>
2018-10-17 01:48:21 +0700

    chore(storybook): added storybook for `@kata-kit/common`

---
+++ packages/common/__stories__/EmptyMessage.story.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { storiesOf, StoryDecorator } from '@storybook/react';
+
+import wInfo from '../../../.storybook/utils/wInfo';
+import Wrapper from '../../../.storybook/components/Wrapper';
+
+import EmptyMessage from '../src/EmptyMessage';
+
+const StoryWrapper: StoryDecorator = storyFn => <Wrapper>{storyFn()}</Wrapper>;
+
+const story = storiesOf('Components/Common/EmptyMessage', module)
+  .addDecorator(StoryWrapper)
+  .addDecorator(wInfo());
+
+const info = `
+## Install
+
+~~~sh
+# yarn
+yarn add @kata-kit/common
+# npm
+npm install @kata-kit/common
+~~~
+
+## Usage
+
+~~~jsx
+import { EmptyMessage } from '@kata-kit/common';
+~~~
+`;
+
+story.add(
+  'Documentation',
+  () => (
+    <EmptyMessage title="Nothing">
+      Nothing to see here. Move along.
+    </EmptyMessage>
+  ),
+  { info }
+);
