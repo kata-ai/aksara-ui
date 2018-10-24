2b1fb26c4483c71ce9f92483fb47805397d522dd
Resi Respati <resir014@gmail.com>
2018-10-24 18:15:47 +0700

    fix(form): add rest props on InputGroup

--- packages/form/src/components/InputGroup.tsx
+++ packages/form/src/components/InputGroup.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-interface InputGroupProps {
+interface InputGroupProps extends React.AllHTMLAttributes<HTMLDivElement> {
+  /** Additional CSS classes to give to the component. */
   className?: string;
 }
 
-const InputGroup: React.SFC<InputGroupProps> = ({ className, children }) => (
-  <Div className={className}>{children}</Div>
+const InputGroup: React.SFC<InputGroupProps> = ({
+  className,
+  children,
+  ...rest
+}) => (
+  <Div className={className} {...rest}>
+    {children}
+  </Div>
 );
 
 export default InputGroup;
