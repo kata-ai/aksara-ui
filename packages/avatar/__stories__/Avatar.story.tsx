7972f0db0e7d01014f18e424d6c8fc148f4e78b0
Resi Respati <resir014@gmail.com>
2018-10-26 21:29:48 +0700

    docs(storybook): add knobs to Avatar

--- packages/avatar/__stories__/Avatar.story.tsx
+++ packages/avatar/__stories__/Avatar.story.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { storiesOf, StoryDecorator } from '@storybook/react';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 import wInfo from '../../../.storybook/utils/wInfo';
 import Wrapper from '../../../.storybook/components/Wrapper';
@@ -10,7 +11,13 @@ const StoryWrapper: StoryDecorator = storyFn => <Wrapper>{storyFn()}</Wrapper>;
 
 const story = storiesOf('Components|Avatar', module)
   .addDecorator(StoryWrapper)
-  .addDecorator(wInfo({ propTables: [Avatar] }));
+  .addDecorator(wInfo({ propTables: [Avatar] }))
+  .addDecorator(withKnobs);
+
+const avatarSizeOptions = {
+  Small: 24,
+  Large: 40
+};
 
 const infoText = `
 ## Install
@@ -45,10 +52,10 @@ import { Badge } from '@kata-kit/badge';
 (story as any).add(
   'Examples',
   () => (
-    <div>
-      <h1>Examples</h1>
-      <Avatar size={40} src="https://picsum.photos/600/400/?image=777" />
-    </div>
+    <Avatar
+      size={select('Size', avatarSizeOptions, 40)}
+      src={text('Message', 'https://picsum.photos/600/400/?image=777')}
+    />
   ),
   {
     info: { disable: true }
