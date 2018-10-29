71fc9c039a012c9829be6926b10261d9d3d861d3
Resi Respati <resir014@gmail.com>
2018-10-29 15:00:24 +0700

    chore: remove unused website files

--- website/src/modules/core/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import { Robot } from '@kata-kit/loading';
-
-const Loading = () => {
-  return <Robot />;
-};
-
-export default Loading;
--- website/src/modules/home/theme.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeAttributes, variables } from '@kata-kit/theme';
-
-const homepageTheme: ThemeAttributes = {
-  backgroundColor: variables.colors.kataBlue,
-  textColor: variables.colors.gray10,
-  headingColor: variables.colors.white
-};
-
-export default homepageTheme;
