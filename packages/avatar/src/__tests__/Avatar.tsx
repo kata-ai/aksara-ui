f356ad47fd893bcc4e72b9ec340d67910ba95216
Resi Respati <resir014@gmail.com>
2018-10-31 16:32:09 +0700

    test(avatar): add avatar tests

---
+++ packages/avatar/src/__tests__/Avatar.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { render } from 'react-testing-library';
+import { Avatar } from '..';
+
+import 'jest-dom/extend-expect';
+import 'jest-styled-components';
+
+describe('Avatar', () => {
+  test('renders on large size by default', () => {
+    const { container } = render(
+      <Avatar src={require('@kata-kit/assets/images/logo-white.svg')} />
+    );
+
+    expect(container.firstChild).toHaveStyleRule('width', '40px');
+    expect(container.firstChild).toHaveStyleRule('height', '40px');
+  });
+
+  test('renders in correct sizes', () => {
+    const { container } = render(
+      <Avatar
+        size={24}
+        src={require('@kata-kit/assets/images/logo-white.svg')}
+      />
+    );
+
+    expect(container.firstChild).toHaveStyleRule('width', '24px');
+    expect(container.firstChild).toHaveStyleRule('height', '24px');
+  });
+});
