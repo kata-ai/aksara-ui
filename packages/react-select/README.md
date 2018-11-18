f0460d7234f38de25a47e2575f48d63684fe272b
Resi Respati <resir014@gmail.com>
2018-11-18 22:39:57 +0700

    feat: react-select (#73)
    New package which acts as a wrapper on top of `react-select` v2.

---
+++ packages/react-select/README.md
@@ -0,0 +1,25 @@
+# @kata-kit/react-select
+
+> kata-kit wrapper for react-select.
+
+## Install
+
+```sh
+# yarn
+yarn add @kata-kit/react-select
+# npm
+npm install @kata-kit/react-select
+```
+
+## Usage
+
+To use this component within your React app, import as follows:
+
+```jsx
+import { SelectBase } from '@kata-kit/react-select';
+import options from './options';
+
+const Component = ({ children }) => (
+  <SelectBase options={options} isClearable />
+);
+```
---
+++ packages/react-select/src/components/SelectAsync.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import { Circle } from '@kata-kit/loading';
+import Select, { Props } from 'react-select/lib/Async';
+import { ValueType, ActionMeta, InputActionMeta } from 'react-select/lib/types';
+import { defaultStyle } from '../styles';
+
+interface DefaultSelectOptions {
+  label: string;
+  value: string;
+}
+
+interface State<TOptions = DefaultSelectOptions> {
+  menuIsOpen: boolean;
+  selected: ValueType<TOptions>;
+  selectedValue?: string;
+}
+
+class SelectAsync<TOptions = DefaultSelectOptions> extends React.Component<
+  Props<TOptions>,
+  State<TOptions>
+> {
+  static defaultProps = {
+    defaultValue: undefined,
+    defaultInputValue: undefined,
+    defaultMenuIsOpen: false
+  };
+
+  constructor(props: Props<TOptions>) {
+    super(props);
+
+    this.state = {
+      menuIsOpen: false,
+      selected: undefined,
+      selectedValue: undefined
+    };
+
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleMenuOpen = this.handleMenuOpen.bind(this);
+    this.handleMenuClose = this.handleMenuClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleMenuOpen() {
+    this.setState({ menuIsOpen: true });
+  }
+
+  handleMenuClose() {
+    this.setState({ menuIsOpen: false });
+  }
+
+  handleInputChange(value: string, actionMeta: InputActionMeta) {
+    if (this.props.onInputChange) {
+      this.props.onInputChange(value, actionMeta);
+    }
+    this.setState({ selectedValue: value });
+  }
+
+  handleChange(value: ValueType<TOptions>, action: ActionMeta) {
+    if (this.props.onChange) {
+      this.props.onChange(value, action);
+    }
+    this.setState({ selected: value });
+  }
+
+  handleBlur(e: React.FocusEvent<HTMLElement>) {
+    if (this.props.onBlur) {
+      this.props.onBlur(e);
+    }
+  }
+
+  render() {
+    const { options, styles, components, ...rest } = this.props;
+    const { menuIsOpen, selected, selectedValue } = this.state;
+    return (
+      <Select
+        value={selected}
+        inputValue={selectedValue}
+        menuIsOpen={menuIsOpen}
+        onMenuOpen={this.handleMenuOpen}
+        onMenuClose={this.handleMenuClose}
+        onBlur={this.handleBlur}
+        onChange={this.handleChange}
+        onInputChange={this.handleInputChange}
+        options={options}
+        styles={{ ...defaultStyle, ...styles }}
+        components={{
+          LoadingIndicator: () => <Circle size={24} />,
+          ...components
+        }}
+        {...rest}
+      />
+    );
+  }
+}
+
+export default SelectAsync;
