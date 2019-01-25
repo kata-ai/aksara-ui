29a2226e9cfe699611323a394b7dcb22f73cad29
Resi Respati <resir014@gmail.com>
2019-01-25 17:44:24 +0700

    test: minor fix to tests

--- packages/common/src/__tests__/Skeleton.test.tsx
+++ packages/common/src/__tests__/Skeleton.test.tsx
@@ -6,19 +6,23 @@ import 'jest-dom/extend-expect';
 import 'jest-styled-components';
 
 describe('Skeleton', () => {
-  test('properly', () => {
+  test('renders properly', () => {
     const { getByTestId } = render(<Skeleton />);
 
     const root = getByTestId('Skeleton-root');
     expect(root).toBeInTheDocument();
   });
 
-  test('renders without title', () => {
-    const { getByTestId } = render(<Skeleton numberOfLines={2} />);
+  test('renders correct number of lines if numberOfLines is set', () => {
+    const numberOfLines = 2;
+    const { getByTestId, getAllByTestId } = render(
+      <Skeleton numberOfLines={numberOfLines} />
+    );
 
-    const root = getByTestId('Skeleton-root');
+    const roots = getAllByTestId('Skeleton-root');
     const multiline = getByTestId('Skeleton-multiline');
-    expect(root).toBeInTheDocument();
     expect(multiline).toBeInTheDocument();
+    expect(roots).toHaveLength(numberOfLines);
+    roots.map(root => expect(root).toBeInTheDocument());
   });
 });
