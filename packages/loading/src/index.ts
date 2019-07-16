5d49d55c2e15124145b713e3e47cdff3186dc28d
Resi Respati <resir014@gmail.com>
2019-07-16 11:33:28 +0700

    fix: fixed building loading

--- packages/loading/src/index.ts
+++ packages/loading/src/index.ts
@@ -1,6 +1,5 @@
 import Circle from './Circle';
 import Robot from './Robot';
-// import LargeSpinner from './LargeSpinner';
 import InfiniteScrollSpinner from './InfiniteScrollSpinner';
 
-export { Robot, Circle, /* LargeSpinner, */ InfiniteScrollSpinner };
+export { Robot, Circle, InfiniteScrollSpinner };
