651d3e44975fcab2f7ecdeffe74048767f4064e8
Fahmi Idris <fahmi@kata.ai>
2019-08-01 13:48:08 +0700

    feat(loading): add color props on loading circle component

--- __stories__/2-components/loading/Loading.story.tsx
+++ __stories__/2-components/loading/Loading.story.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { storiesOf, StoryDecorator } from '@storybook/react';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 
 import RootWrapper from '@storybook-utils/components/Wrapper';
 
@@ -11,12 +12,20 @@ const StoryWrapper: StoryDecorator = storyFn => (
   <RootWrapper>{storyFn()}</RootWrapper>
 );
 
-const story = storiesOf('Components|Loading', module).addDecorator(
-  StoryWrapper
-);
+const story = storiesOf('Components|Loading', module)
+  .addDecorator(StoryWrapper)
+  .addDecorator(withKnobs);
 
 story.add('Circle', () => <Circle />);
 
 story.add('Robot', () => <Robot />);
 
 story.add('InfiniteScrollSpinner', () => <InfiniteScrollSpinner />);
+
+story.add(
+  'Examples',
+  () => <Circle size={number('Size', 40)} color={text('color', '#006fe6')} />,
+  {
+    info: { disable: true }
+  }
+);
