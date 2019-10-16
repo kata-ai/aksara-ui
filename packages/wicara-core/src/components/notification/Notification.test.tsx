765f9c07eb3d4b79b2b0d97757213b749dfd9c8c
Resi Respati <resir014@gmail.com>
2019-10-16 17:21:18 +0700

    test(notification): add tests for toaster

---
+++ packages/wicara-core/src/components/notification/Notification.test.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { notification } from './index';
+
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components';
+import { Button } from '../button';
+
+describe('components/Notification', () => {
+  describe('toaster', () => {
+    test('renders the portal element', () => {
+      const { baseElement } = render(
+        <Button
+          variant="primary"
+          onClick={() =>
+            notification.toaster({
+              title: 'Toaster',
+              message: 'Hello world! This is a toaster',
+              status: 'success',
+              dismissible: true,
+              dismissAfter: 5000
+            })
+          }
+        >
+          Push Me
+        </Button>
+      );
+
+      const element = baseElement.querySelector("div[data-wicara-notification-container='']");
+      expect(element).toBeInTheDocument();
+    });
+
+    test('renders notification when method is called', () => {
+      jest.useFakeTimers();
+
+      const { getAllByText, baseElement } = render(
+        <Button
+          variant="primary"
+          onClick={() =>
+            notification.toaster({
+              id: 'test-toaster',
+              title: 'Toaster',
+              message: 'Hello world! This is a toaster',
+              status: 'success',
+              dismissible: true,
+              dismissAfter: 5000
+            })
+          }
+        >
+          Push Me
+        </Button>
+      );
+
+      fireEvent.click(getAllByText('Push Me')[0]);
+
+      setTimeout(() => {
+        const element = baseElement.querySelector('#test-toaster-1');
+        expect(element).toBeInTheDocument();
+        expect(element).toHaveAttribute('data-state', 'entered');
+      }, 1000);
+
+      jest.runAllTimers();
+    });
+  });
+});
