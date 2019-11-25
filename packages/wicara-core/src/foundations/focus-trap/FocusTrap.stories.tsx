0281b03b76b22962347345d2690f6cbecf06d746
Resi Respati <resir014@gmail.com>
2019-11-25 14:30:39 +0700

    docs(focus-trap): added storybook + readme to FocusTrap

---
+++ packages/wicara-core/src/foundations/focus-trap/FocusTrap.stories.tsx
@@ -0,0 +1,54 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import { StorybookWrapper, StoryContainer, StoryHeader } from '../../utils/storybook';
+import FocusTrap from './FocusTrap';
+
+const readme = require('./README.md');
+
+const story = storiesOf('Core|Foundations/FocusTrap', module).addDecorator(StorybookWrapper);
+
+story.add(
+  'default',
+  () => {
+    const [active, setActive] = React.useState(false);
+
+    return (
+      <StoryContainer>
+        <StoryHeader
+          title="FocusTrap"
+          subtitle="Helper component to support trapping tab focus inside a component."
+        />
+        <p>
+          Tubulo putas dicere? Erat enim Polemonis. Minime vero istorum quidem, inquit. Hic nihil
+          fuit, quod quaereremus.{' '}
+          <a href="https://www.youtube.com/watch?v=mUGDxyG1kOI" target="_blank">
+            Est, ut dicis, inquit;
+          </a>{' '}
+          Iam contemni non poteris.{' '}
+        </p>
+
+        <FocusTrap active={active}>
+          <p>
+            Focus Lock: <strong>{active ? 'On' : 'Off'}</strong>
+          </p>
+          <button onClick={() => setActive(!active)} style={{ marginBottom: '16px' }}>
+            Toggle focus trap
+          </button>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quid adiuvas? Illa tamen
+            simplicia, vestra versuta.{' '}
+            <a href="https://www.youtube.com/watch?v=lOiR7b6jumI" target="_blank">
+              Quamquam tu hanc copiosiorem etiam soles dicere.
+            </a>{' '}
+            Sed ego in hoc resisto; Si longus, levis. Roges enim Aristonem, bonane ei videantur
+            haec: vacuitas doloris, divitiae, valitudo;{' '}
+          </p>
+        </FocusTrap>
+      </StoryContainer>
+    );
+  },
+  {
+    notes: { markdown: readme }
+  }
+);
