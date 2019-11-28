9995068267e69d2c1b244058a750391a149ec3e0
Resi Respati <resir014@gmail.com>
2019-11-28 15:05:52 +0700

    test(form): add tests for form

---
+++ packages/wicara-core/src/components/form/Form.test.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import {
+  FormGroup,
+  FormLabel,
+  InputText,
+  InputTextarea,
+  InputGroup,
+  InputAddon,
+  FormHelp,
+  FormError
+} from './components';
+
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components';
+
+describe('components/Form', () => {
+  test('renders standard text input correctly', () => {
+    const { getByTestId } = render(
+      <FormGroup>
+        <FormLabel htmlFor="textDummy">Text input</FormLabel>
+        <InputText data-testid="text-input" placeholder="Type here..." />
+        <FormHelp>Some help text.</FormHelp>
+      </FormGroup>
+    );
+
+    const input = getByTestId('text-input');
+    expect(input).toBeInTheDocument();
+  });
+
+  test('renders textarea correctly', () => {
+    const { getByTestId } = render(
+      <FormGroup>
+        <FormLabel htmlFor="textarea">Text area</FormLabel>
+        <InputTextarea data-testid="text-input" rows={4} placeholder="Type here..." />
+      </FormGroup>
+    );
+
+    const input = getByTestId('text-input');
+    expect(input).toBeInTheDocument();
+  });
+
+  test('renders error state correctly', () => {
+    const { getByTestId } = render(
+      <FormGroup>
+        <FormLabel data-testid="label-error" errors>
+          Email Address
+        </FormLabel>
+        <InputText errors placeholder="Type here..." />
+        <FormError data-testid="input-error">Please enter an email address.</FormError>
+      </FormGroup>
+    );
+
+    const input = getByTestId('input-error');
+
+    expect(input).toHaveTextContent('Please enter an email address.');
+  });
+
+  test('renders disabled input correctly', () => {
+    const { getByTestId } = render(
+      <FormGroup>
+        <FormLabel htmlFor="disabled">Disabled input</FormLabel>
+        <InputText data-testid="disabled-input" disabled placeholder="Can't touch this" />
+      </FormGroup>
+    );
+
+    const input = getByTestId('disabled-input');
+    expect(input).toHaveAttribute('disabled');
+  });
+
+  test('renders input addon correctly', () => {
+    const { getByTestId } = render(
+      <FormGroup>
+        <InputGroup>
+          <InputAddon data-testid="input-addon">addon text</InputAddon>
+          <InputText placeholder="Type here..." addon />
+        </InputGroup>
+      </FormGroup>
+    );
+
+    const addon = getByTestId('input-addon');
+    expect(addon).toHaveTextContent('addon text');
+  });
+});
