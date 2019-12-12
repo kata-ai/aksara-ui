07ff867994469dfbfbce66c5b5713feebf0f5c0b
Resi Respati <resir014@gmail.com>
2019-12-12 12:03:59 +0700

    style(reset): added warning message for deprecated use

--- packages/wicara-core/src/foundations/reset/components/WicaraProvider.tsx
+++ packages/wicara-core/src/foundations/reset/components/WicaraProvider.tsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 import { Theme } from '../../../Theme';
 import GlobalStyles from './GlobalStyles';
 
-interface ResetProps {
+interface WicaraProviderProps {
   className?: string;
   style?: React.CSSProperties;
   /** Import the provider without any style resets. */
   noReset?: boolean;
 }
 
-const WicaraProvider: React.FC<ResetProps> = ({ children, noReset }) => {
+const WicaraProvider: React.FC<WicaraProviderProps> = ({ children, noReset }) => {
   return (
     <Theme>
       {!noReset && <GlobalStyles />}
@@ -26,6 +26,12 @@ WicaraProvider.defaultProps = {
 /**
  * @deprecated This has been replaced by `WicaraProvider`.
  */
-export const WicaraReset = WicaraProvider;
+export const WicaraReset: React.FC<WicaraProviderProps> = ({ ...rest }) => {
+  console.warn(
+    '`WicaraReset` has been deprecated in favour of `WicaraProvider`. Please use that instead.'
+  );
+
+  return <WicaraProvider {...rest} />;
+};
 
 export default WicaraProvider;
