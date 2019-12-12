ffa2bf95cdc9ca4118685b57c9c449061e1a0c3e
Resi Respati <resir014@gmail.com>
2019-12-12 10:31:40 +0700

    chore: move rollup config to their respective packages (#166)

---
+++ packages/wicara-core/rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import autoExternal from 'rollup-plugin-auto-external';
+import pkg from './package.json';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    autoExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      typescript: require('typescript'),
+      tsconfigDefaults: {
+        compilerOptions: {
+          sourceMap: true,
+          declaration: true,
+          jsx: 'react'
+        }
+      }
+    })
+  ]
+};
