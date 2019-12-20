117dd797a0f5321f4e563e29610fa4c065b7f4a2
Resi Respati <resir014@gmail.com>
2019-12-20 13:18:55 +0700

    fix(card): use type instead of interface

--- packages/wicara-core/src/foundations/card/components/Card.tsx
+++ packages/wicara-core/src/foundations/card/components/Card.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { shadow, ShadowProps } from 'styled-system';
 import { Box } from '../../box';
 
-export interface CardProps extends ShadowProps {}
+export type CardProps = ShadowProps;
 
 /**
  * Card is similar to `Box`, but with additional `shadow` and `border` styled-system hooks.
