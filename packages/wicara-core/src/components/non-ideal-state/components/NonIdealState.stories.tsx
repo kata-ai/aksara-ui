ad3d9ad10972cc913812d3a4bf6bc14e8a102a23
Resi Respati <resir014@gmail.com>
2020-01-26 23:17:36 +0700

    docs: rest of components

--- packages/wicara-core/src/components/non-ideal-state/components/NonIdealState.stories.tsx
+++ packages/wicara-core/src/components/non-ideal-state/components/NonIdealState.stories.tsx
@@ -1,32 +1,34 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { StoryContainer, StoryHeader, StorybookWrapper } from '../../../utils/storybook';
+import { SystemWrapper, SystemHeader, SystemContainer } from '../../../utils/storybook';
 
 import { Paragraph } from '../../../foundations';
 import NonIdealState from './NonIdealState';
 
 const readme = require('../README.md');
 
-const story = storiesOf('Core|Components/Non-Ideal State', module).addDecorator(StorybookWrapper);
+const story = storiesOf('Core|Components/Non-Ideal State', module).addDecorator(SystemWrapper);
 
 story.add(
   'default',
   () => (
-    <StoryContainer>
-      <StoryHeader
+    <>
+      <SystemHeader
         title="Non-Ideal State"
         subtitle="Used to inform users that some contents are unavailable."
       />
-      <NonIdealState
-        image={<img src="https://via.placeholder.com/318x310.png" alt="No Project Selected" />}
-        title="No Project Selected"
-      >
-        <Paragraph m={0}>
-          You have not selected a project. Please select one from the dropdown on the left.
-        </Paragraph>
-      </NonIdealState>
-    </StoryContainer>
+      <SystemContainer>
+        <NonIdealState
+          image={<img src="https://via.placeholder.com/318x310.png" alt="No Project Selected" />}
+          title="No Project Selected"
+        >
+          <Paragraph m={0}>
+            You have not selected a project. Please select one from the dropdown on the left.
+          </Paragraph>
+        </NonIdealState>
+      </SystemContainer>
+    </>
   ),
   {
     notes: {
--- packages/wicara-core/src/components/notification/Notification.stories.tsx
+++ packages/wicara-core/src/components/notification/Notification.stories.tsx
@@ -2,10 +2,10 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import {
-  StoryContainer,
-  StoryHeader,
-  StorybookWrapper,
-  ComponentBlock
+  ComponentBlock,
+  SystemWrapper,
+  SystemHeader,
+  SystemContainer
 } from '../../utils/storybook';
 import { Button } from '../button';
 import { notification } from './index';
@@ -13,91 +13,93 @@ import { notification } from './index';
 const readme = require('./README.md');
 
 storiesOf('Core|Components/Notifications', module)
-  .addDecorator(StorybookWrapper)
+  .addDecorator(SystemWrapper)
   .add(
     'basic',
     () => (
-      <StoryContainer>
-        <StoryHeader
+      <>
+        <SystemHeader
           title="Notifications"
           subtitle="Pluggable notifications element that doesn't rely on Redux."
         />
-        <ComponentBlock title="Toaster">
-          <Button
-            variant="support"
-            onClick={() => {
-              notification.toaster({
-                title: 'Toaster',
-                message: 'Hello world! This is a toaster',
-                dismissible: true,
-                dismissAfter: 5000
-              });
-            }}
-            style={{ marginRight: 8 }}
-          >
-            Default
-          </Button>
-          <Button
-            variant="support"
-            onClick={() => {
-              notification.toaster({
-                title: 'Toaster',
-                message: 'Hello world! This is a toaster',
-                status: 'success',
-                dismissible: true,
-                dismissAfter: 5000
-              });
-            }}
-            style={{ marginRight: 8 }}
-          >
-            Success
-          </Button>
-          <Button
-            variant="support"
-            onClick={() => {
-              notification.toaster({
-                title: 'Toaster',
-                message: 'Hello world! This is a toaster',
-                status: 'info',
-                dismissible: true,
-                dismissAfter: 5000
-              });
-            }}
-            style={{ marginRight: 8 }}
-          >
-            Info
-          </Button>
-          <Button
-            variant="support"
-            onClick={() => {
-              notification.toaster({
-                title: 'Toaster',
-                message: 'Hello world! This is a toaster',
-                status: 'warning',
-                dismissible: true,
-                dismissAfter: 5000
-              });
-            }}
-            style={{ marginRight: 8 }}
-          >
-            Warning
-          </Button>
-          <Button
-            variant="support"
-            onClick={() => {
-              notification.toaster({
-                title: 'Toaster',
-                message: 'Hello world! This is a toaster',
-                status: 'error',
-                dismissible: true,
-                dismissAfter: 5000
-              });
-            }}
-          >
-            Error
-          </Button>
-        </ComponentBlock>
-      </StoryContainer>
+        <SystemContainer>
+          <ComponentBlock title="Toaster">
+            <Button
+              variant="support"
+              onClick={() => {
+                notification.toaster({
+                  title: 'Toaster',
+                  message: 'Hello world! This is a toaster',
+                  dismissible: true,
+                  dismissAfter: 5000
+                });
+              }}
+              style={{ marginRight: 8 }}
+            >
+              Default
+            </Button>
+            <Button
+              variant="support"
+              onClick={() => {
+                notification.toaster({
+                  title: 'Toaster',
+                  message: 'Hello world! This is a toaster',
+                  status: 'success',
+                  dismissible: true,
+                  dismissAfter: 5000
+                });
+              }}
+              style={{ marginRight: 8 }}
+            >
+              Success
+            </Button>
+            <Button
+              variant="support"
+              onClick={() => {
+                notification.toaster({
+                  title: 'Toaster',
+                  message: 'Hello world! This is a toaster',
+                  status: 'info',
+                  dismissible: true,
+                  dismissAfter: 5000
+                });
+              }}
+              style={{ marginRight: 8 }}
+            >
+              Info
+            </Button>
+            <Button
+              variant="support"
+              onClick={() => {
+                notification.toaster({
+                  title: 'Toaster',
+                  message: 'Hello world! This is a toaster',
+                  status: 'warning',
+                  dismissible: true,
+                  dismissAfter: 5000
+                });
+              }}
+              style={{ marginRight: 8 }}
+            >
+              Warning
+            </Button>
+            <Button
+              variant="support"
+              onClick={() => {
+                notification.toaster({
+                  title: 'Toaster',
+                  message: 'Hello world! This is a toaster',
+                  status: 'error',
+                  dismissible: true,
+                  dismissAfter: 5000
+                });
+              }}
+            >
+              Error
+            </Button>
+          </ComponentBlock>
+        </SystemContainer>
+      </>
     ),
     {
       notes: { markdown: readme }
