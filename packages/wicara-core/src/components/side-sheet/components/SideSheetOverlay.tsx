3dcca1f1beb20d20904354958f4f6cad7a51a106
Resi Respati <resir014@gmail.com>
2020-02-07 19:01:32 +0700

    fix(side-sheet): fix overlay background color

--- packages/wicara-core/src/components/side-sheet/components/SideSheetOverlay.tsx
+++ packages/wicara-core/src/components/side-sheet/components/SideSheetOverlay.tsx
@@ -1,6 +1,8 @@
 import styled, { keyframes } from 'styled-components';
+import { transparentize } from 'polished';
 
 import { ANIMATION_DURATION } from '../../notification/utils/constants';
+import { colors } from '../../../utils';
 
 const FadeIn = keyframes`
   0% {
@@ -41,7 +43,7 @@ const SideSheetOverlay = styled('div')`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.65);
+    background-color: ${transparentize(0.35, colors.grey09)};
     content: ' ';
     visibility: hidden;
     opacity: 0;
