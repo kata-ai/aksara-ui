c52ec826d5158f158099deb5dff13018e00c3d69
Resi Respati <resir014@gmail.com>
2020-02-07 15:33:01 +0700

    feat(common): added Stack and Inline components

---
+++ packages/wicara-core/src/foundations/common/components/Stack/Stack.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { Space } from '../../../../Theme';
+import { Box, BoxProps } from '../../../box';
+
+export interface StackProps extends Omit<BoxProps, 'color'> {
+  id?: string;
+  className?: string;
+  style?: React.CSSProperties;
+  spacing?: Space;
+  [key: string]: any;
+}
+
+const Stack: React.FC<StackProps> = ({ children, spacing, ...rest }) => {
+  const validChildrenArray = React.Children.toArray(children).filter(React.isValidElement);
+
+  return (
+    <Box {...rest}>
+      {validChildrenArray.map((child, index) => {
+        const isLastChild = validChildrenArray.length === index + 1;
+        const spacingProps = { mb: isLastChild ? null : spacing };
+        return React.cloneElement(child, spacingProps);
+      })}
+    </Box>
+  );
+};
+
+Stack.displayName = 'Stack';
+
+export default Stack;
