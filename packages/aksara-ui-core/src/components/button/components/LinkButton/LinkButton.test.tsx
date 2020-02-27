4f010c798ec9c8769ac2b476a8c42e35d623bdd9
Resi Respati <resir014@gmail.com>
2020-02-27 12:12:52 +0700

    test: fixed test

--- packages/aksara-ui-core/src/components/button/components/LinkButton/LinkButton.test.tsx
+++ packages/aksara-ui-core/src/components/button/components/LinkButton/LinkButton.test.tsx
@@ -16,7 +16,7 @@ describe('components/Button', () => {
     test('renders colors correctly by variant', () => {
       const { container } = render(<LinkButton variant="primary">test button</LinkButton>);
 
-      expect(container.firstChild).toHaveStyleRule('color', colors.blue05);
+      expect(container.firstChild).toHaveStyleRule('color', colors.blue06);
     });
 
     test('renders icons correctly', () => {
