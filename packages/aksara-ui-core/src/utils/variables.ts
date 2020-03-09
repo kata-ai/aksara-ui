66a6e0cc002538ef8e39e039bc3e16f1314bd100
Resi Respati <resir014@gmail.com>
2020-03-09 13:07:46 +0700

    fix(utils): rename brand fonts token to `brand`

--- packages/aksara-ui-core/src/utils/variables.ts
+++ packages/aksara-ui-core/src/utils/variables.ts
@@ -241,7 +241,7 @@ export const systemFonts =
 
 export const fonts = {
   system: systemFonts,
-  barlow: `'Barlow', ${systemFonts}`,
+  brand: `'Barlow', ${systemFonts}`,
   monospace: "'SF Mono', Inconsolata, Menlo, Monaco, Consolas, 'Courier New', Courier, monospace;",
 };
 
