a91329980321e184c6d1ab7fd0ce87c82332bd99
Resi Respati <resir014@gmail.com>
2020-05-14 13:36:41 +0700

    test(interactive-card): add some tests

--- packages/aksara-ui-core/src/components/interactive-card/components/InteractiveCard/InteractiveCard.test.tsx
+++ packages/aksara-ui-core/src/components/interactive-card/components/InteractiveCard/InteractiveCard.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { render } from '@testing-library/react';
 
 import InteractiveCard from './InteractiveCard';
+import { Button } from '../../../button';
 
 describe('components/InteractiveCard', () => {
   describe('<InteractiveCard />', () => {
@@ -9,6 +10,14 @@ describe('components/InteractiveCard', () => {
       const { container } = render(<InteractiveCard>henlo</InteractiveCard>);
 
       expect(container.firstChild).toBeInTheDocument();
+    });
+
+    test('renders icons correctly', () => {
+      const { getByText } = render(
+        <InteractiveCard actions={<Button size={32}>action</Button>}>test card</InteractiveCard>
+      );
+
+      expect(getByText('action')).toBeInTheDocument();
     });
   });
 });
