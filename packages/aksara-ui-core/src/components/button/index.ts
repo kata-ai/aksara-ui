2fac2f9cbe22b7c51f8cc8de3531c62d1ec0c99d
Resi Respati <resir014@gmail.com>
2020-05-15 14:50:02 +0700

    fix(button): remove FloatingButton imports from index

--- packages/aksara-ui-core/src/components/button/index.ts
+++ packages/aksara-ui-core/src/components/button/index.ts
@@ -1,9 +1,6 @@
 export { default as Button } from './Button';
 export * from './Button';
 
-export { default as FloatingButton } from './FloatingButton';
-export * from './FloatingButton';
-
 export * from './components/IconButton';
 export * from './components/LinkButton';
 export * from './styles';
