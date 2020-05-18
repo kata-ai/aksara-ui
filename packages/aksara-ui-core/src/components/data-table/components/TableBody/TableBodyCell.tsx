211d07ce114105fa1671a826a40346681d8e2dbd
Resi Respati <resir014@gmail.com>
2020-05-18 15:12:24 +0700

    feat(data-table): allow custom content on table cell (#256)

--- packages/aksara-ui-core/src/components/data-table/components/TableBody/TableBodyCell.tsx
+++ packages/aksara-ui-core/src/components/data-table/components/TableBody/TableBodyCell.tsx
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 import { Text } from '../../../../foundations';
 import { BaseStylesProps, BaseStyles } from '../../styles';
 
-export type TableBodyCellProps = React.ThHTMLAttributes<HTMLTableHeaderCellElement>;
+export interface TableBodyCellProps extends React.TdHTMLAttributes<HTMLTableCellElement> {
+  /** Allow styling of table cell element. This removes the base text styling. */
+  customContent?: boolean;
+}
 
 const TableBodyRoot = styled('td')<BaseStylesProps>`
   ${BaseStyles}
@@ -11,12 +14,22 @@ const TableBodyRoot = styled('td')<BaseStylesProps>`
 
 const TableBodyText = styled(Text)``;
 
-const TableBodyCell: React.FC<TableBodyCellProps> = ({ className, style, children, ...rest }) => {
-  return (
-    <TableBodyRoot className={className} style={style} px="md" py="md" {...rest}>
+const TableBodyCell: React.FC<TableBodyCellProps> = ({ className, style, children, customContent, ...rest }) => {
+  const renderContent = () => {
+    if (customContent) {
+      return children;
+    }
+
+    return (
       <TableBodyText scale={200} fontWeight={600}>
         {children}
       </TableBodyText>
+    );
+  };
+
+  return (
+    <TableBodyRoot className={className} style={style} px="md" py="md" {...rest}>
+      {renderContent()}
     </TableBodyRoot>
   );
 };
