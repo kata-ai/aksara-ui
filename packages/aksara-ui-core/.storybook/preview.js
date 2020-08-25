aa3b0ebd5cbc61748d92803020bedfb244d7f447
Resi Respati <resir014@gmail.com>
2020-08-25 12:03:06 +0700

    fix: actually remove unused addons from config

--- packages/aksara-ui-core/.storybook/preview.js
+++ packages/aksara-ui-core/.storybook/preview.js
@@ -1,22 +1,5 @@
-import { addDecorator, addParameters } from '@storybook/react';
-import { withA11y } from '@storybook/addon-a11y';
-import { jsxDecorator } from 'storybook-addon-jsx';
+import { addParameters } from '@storybook/react';
 import storybookTheme from './storybookTheme';
-
-// Accessibility addon
-// https://github.com/storybooks/storybook/tree/master/addons/a11y
-if (process.env.NODE_ENV === 'development') {
-  addDecorator(withA11y);
-}
-
-// JSX addon
-// https://github.com/storybooks/addon-jsx
-addDecorator(jsxDecorator);
-addParameters({
-  jsx: {
-    showDefaultProps: false,
-  },
-});
 
 // use custom theme
 addParameters({
