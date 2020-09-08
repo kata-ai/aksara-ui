b924e0cbcf63057236275f624f92c31e2c836998
Resi Respati <resir014@gmail.com>
2020-09-08 16:58:07 +0700

    feat(panel): panel component
    Closes #267.

---
+++ packages/aksara-ui-core/src/components/panel/components/Panel.stories.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Story } from '@storybook/react';
+import { IconChevronLeft } from '@aksara-ui/icons';
+
+import { Text, Box } from '../../../foundations';
+import { IconButton } from '../../button';
+import Panel, { PanelProps } from './Panel';
+import PanelHeader from './PanelHeader';
+import PanelFooter from './PanelFooter';
+
+export default {
+  title: 'Core/Components/Panel/Panel',
+  component: Panel,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['sm', 'lg'],
+      },
+    },
+  },
+};
+
+const Template: Story<PanelProps> = args => (
+  <Panel {...args}>
+    <PanelHeader title="Panel Title" />
+    <Box flex="1 1 auto">inner</Box>
+    <PanelFooter>
+      <IconButton size={32}>
+        <IconChevronLeft fill="currentColor" aria-hidden="true" />
+      </IconButton>
+      <Text ml="md" scale={200} fontWeight={500}>
+        Collapse
+      </Text>
+    </PanelFooter>
+  </Panel>
+);
+
+export const Example = Template.bind({});
+Example.args = {
+  size: 'sm',
+};
---
+++ packages/aksara-ui-core/src/components/panel/components/Panel.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Box, BoxProps } from '../../../foundations';
+
+export type PanelSizes = 'sm' | 'lg';
+
+export interface PanelProps extends Omit<BoxProps, 'width' | 'maxWidth' | 'backgroundColor'> {
+  size?: PanelSizes;
+}
+
+const Panel: React.FC<PanelProps> = ({ children, size, ...rest }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="100%"
+      maxWidth={size === 'sm' ? 280 : 400}
+      height="100%"
+      backgroundColor="grey01"
+      {...rest}
+    >
+      {children}
+    </Box>
+  );
+};
+
+Panel.defaultProps = {
+  size: 'sm',
+};
+
+export default Panel;
