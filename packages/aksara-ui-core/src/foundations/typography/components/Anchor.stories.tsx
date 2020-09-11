635a5c69397a377b2f1bfbfe35126d3feee26539
Resi Respati <resir014@gmail.com>
2020-09-11 13:16:25 +0700

    chore: added args for storybook text components (#283)

--- packages/aksara-ui-core/src/foundations/typography/components/Anchor.stories.tsx
+++ packages/aksara-ui-core/src/foundations/typography/components/Anchor.stories.tsx
@@ -2,16 +2,23 @@ import * as React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Story } from '@storybook/react';
 
+import { fonts } from '../../../utils';
 import Anchor, { AnchorProps } from './Anchor';
 
 export default {
-  title: 'Core/Foundations/Anchor',
+  title: 'Core/Foundations/Typography/Anchor',
   component: Anchor,
   argTypes: {
     scale: {
       control: {
         type: 'select',
         options: [200, 300, 400, 500],
+      },
+    },
+    fontFamily: {
+      control: {
+        type: 'select',
+        options: Object.keys(fonts),
       },
     },
   },
@@ -27,4 +34,5 @@ export const Example = Template.bind({});
 Example.args = {
   href: 'https://www.youtube.com/watch?v=P_mQpbCSQOo',
   scale: 400,
+  fontFamily: 'system',
 };
