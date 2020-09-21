bdba0b6142d604aaf684d06642d8c2b3925499fa
Resi Respati <resir014@gmail.com>
2020-09-21 15:49:28 +0700

    chore: convert more stories to storybook args (#285)

--- packages/aksara-ui-core/src/components/button/components/IconButton/IconButtonAnchor.stories.tsx
+++ packages/aksara-ui-core/src/components/button/components/IconButton/IconButtonAnchor.stories.tsx
@@ -1,35 +1,43 @@
 import { action } from '@storybook/addon-actions';
+import { Story } from '@storybook/react';
 import * as React from 'react';
 import { IconPen } from '@aksara-ui/icons';
-import { select, text } from '@storybook/addon-knobs';
-import { SelectTypeOptionsProp } from '@storybook/addon-knobs/dist/components/types';
 
-import { IconButtonSizes, IconButtonVariants } from './styles';
-import IconButtonAnchor from './IconButtonAnchor';
-
-const buttonVariants: SelectTypeOptionsProp<IconButtonVariants> = {
-  default: 'default',
-  outline: 'outline',
-  destructive: 'destructive',
-  ghost: 'ghost',
-};
-const buttonSizes: SelectTypeOptionsProp<IconButtonSizes> = { 24: 24, 32: 32, 40: 40 };
+import IconButtonAnchor, { IconButtonAnchorProps } from './IconButtonAnchor';
 
 export default {
   title: 'Core/Components/Button/IconButtonAnchor',
   component: IconButtonAnchor,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: ['default', 'outline', 'destructive', 'ghost'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: [24, 32, 40],
+      },
+    },
+    href: {
+      control: 'text',
+    },
+    target: {
+      control: 'text',
+    },
+  },
 };
 
-export const Example = () => (
-  <IconButtonAnchor
-    aria-label="Push Me"
-    variant={select<IconButtonVariants>('Variant', buttonVariants, 'outline')}
-    size={select<IconButtonSizes>('Size', buttonSizes, 32)}
-    href={text('URL', 'https://www.youtube.com/watch?v=fdixQDPA2h0')}
-    target="_blank"
-    rel="noopener noreferrer"
-    onClick={action('button-click')}
-  >
+export const Example: Story<IconButtonAnchorProps> = args => (
+  <IconButtonAnchor aria-label="Push Me" rel="noopener noreferrer" onClick={action('button-click')} {...args}>
     <IconPen fill="currentColor" aria-hidden="true" />
   </IconButtonAnchor>
 );
+Example.args = {
+  variant: 'outline',
+  size: 32,
+  href: 'https://www.youtube.com/watch?v=fdixQDPA2h0',
+  target: '_blank',
+};
