a572ce82bb4db80be4566fa1cbd589e4910be498
Resi Respati <resir014@gmail.com>
2020-10-05 17:19:08 +0700

    test(dropdown): fixed dropdown tests

--- packages/aksara-ui-core/src/components/dropdown/components/DropdownMenuItem/DropdownMenuHeader.test.tsx
+++ packages/aksara-ui-core/src/components/dropdown/components/DropdownMenuItem/DropdownMenuHeader.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { DropdownMenuDivider } from '.';
+import { DropdownMenuHeader } from '.';
 
 describe('components/Dropdown', () => {
   describe('<DropdownMenuDivider />', () => {
     test('renders correctly', () => {
-      const { container } = render(<DropdownMenuDivider />);
+      const { container } = render(<DropdownMenuHeader>title</DropdownMenuHeader>);
 
       expect(container.firstChild).toBeInTheDocument();
     });
