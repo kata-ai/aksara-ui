bdd8ace52e22742f1b5ded3edac021f51bdd67be
Resi Respati <resir014@gmail.com>
2020-10-07 12:42:25 +0700

    refactor(portal): `data-wicara-portal` -> `data-aksara-portal`
    BREAKING CHANGE: Any DOM query that calls `data-wicara-portal` will need
    to be changed to the new data-attribute.

--- packages/aksara-ui-core/src/components/dialog/components/Dialog.test.tsx
+++ packages/aksara-ui-core/src/components/dialog/components/Dialog.test.tsx
@@ -36,7 +36,7 @@ describe('components/Dialog', () => {
         </Dialog>
       );
 
-      const overlay = document.querySelector('[data-wicara-portal] > div');
+      const overlay = document.querySelector('[data-aksara-portal] > div');
       if (overlay) {
         fireEvent.click(overlay);
         expect(handleClose).toHaveBeenCalledTimes(1);
@@ -53,7 +53,7 @@ describe('components/Dialog', () => {
         </Dialog>
       );
 
-      const overlay = document.querySelector('[data-wicara-portal] > div');
+      const overlay = document.querySelector('[data-aksara-portal] > div');
       if (overlay) {
         fireEvent.click(overlay);
         expect(handleClose).toHaveBeenCalledTimes(0);
