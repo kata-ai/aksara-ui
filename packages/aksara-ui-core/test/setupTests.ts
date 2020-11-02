26e5eef03407b69d3e5a54e2cc58657aa431c6eb
Resi Respati <resir014@gmail.com>
2020-11-02 10:01:47 +0700

    chore: fixed build stuff, add vercel config

--- packages/aksara-ui-core/test/setupTests.ts
+++ packages/aksara-ui-core/test/setupTests.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 /* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-styled-components';
 
