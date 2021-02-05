2199f677e7d4249d18e86d59032657e3ee806c04
Resi Respati <resir014@gmail.com>
2021-02-05 15:00:42 +0700

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,8 +26,27 @@ module.exports = {
     'react/jsx-one-expression-per-line': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-wrap-multilines': 'off',
+
     // Cannot use `Component.displayName` pattern on class components in TS.
     'react/static-property-placement': 'off',
+
+    // Require or disallow a space immediately following the // or /* in a comment
+    // https://eslint.org/docs/rules/spaced-comment
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        line: {
+          exceptions: ['-', '+'],
+          markers: ['=', '!', '#region', '#endregion', '/'], // space here to support sprockets directives and typescript reference comments
+        },
+        block: {
+          exceptions: ['-', '+'],
+          markers: ['=', '!', '#region', '#endregion', ':', '::'], // space here to support sprockets directives and flow comment types
+          balanced: true,
+        },
+      },
+    ],
   },
   settings: {
     'import/resolver': {
@@ -46,10 +65,24 @@ module.exports = {
         warnOnUnsupportedTypeScriptVersion: true,
       },
       rules: {
+        // Allow unused variables that starts with, or is `_`
         'no-unused-vars': 'off',
-        '@typescript-eslint/camelcase': 'off',
-        '@typescript-eslint/explicit-function-return-type': ['off'],
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            args: 'after-used',
+            ignoreRestSiblings: true,
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+          },
+        ],
+
+        // Override the base rules so to not clash with each other.
+        // https://github.com/typescript-eslint/typescript-eslint/issues/2540
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error', { functions: true, classes: true, variables: true }],
+
         // Allow `.ts` and `.tsx` extensions to be omitted
         'import/extensions': [
           'error',
@@ -62,6 +95,7 @@ module.exports = {
             tsx: 'never',
           },
         ],
+
         // Extend this config for TypeScript files
         'import/no-extraneous-dependencies': [
           'error',
@@ -83,8 +117,12 @@ module.exports = {
             optionalDependencies: false,
           },
         ],
+
         // No need to use prop types on TS files.
         'react/prop-types': 'off',
+
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/explicit-function-return-type': ['off'],
       },
     },
     {
