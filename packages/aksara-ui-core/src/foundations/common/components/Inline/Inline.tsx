17de6b1f23accfaceba20efca241debb0fe0322d
Resi Respati <resir014@gmail.com>
2021-02-22 15:59:03 +0700

    fix(inline): fixed incorrect margin being applied

--- packages/aksara-ui-core/src/foundations/common/components/Inline/Inline.tsx
+++ packages/aksara-ui-core/src/foundations/common/components/Inline/Inline.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/no-array-index-key */
 import * as React from 'react';
-import { themeGet } from '@styled-system/theme-get';
+import { get } from 'styled-system';
 
-import { theme } from '../../../../theme';
+import { useTheme, Theme } from '../../../../theme';
 import { Box, BoxProps } from '../../../box';
 
-type Space = keyof typeof theme['space'];
+type Space = keyof Theme['space'];
 
 interface SpacingProps {
   spacing?: Space;
@@ -19,16 +19,14 @@ export interface InlineProps extends Omit<BoxProps, 'color'>, SpacingProps {
 }
 
 const Inline: React.FC<InlineProps> = ({ children, spacing = 'sm', alignItems, ...rest }) => {
+  const theme = useTheme();
   const validChildrenArray = React.Children.toArray(children).filter(React.isValidElement);
 
+  const negativeSpacing = React.useMemo(() => `-${get(theme, `space.${spacing}`, 0)}px`, [theme]);
+
   return (
-    <Box marginTop={`-${themeGet(`space.${spacing}`, 0)({ spacing })}px`} {...rest}>
-      <Box
-        display="flex"
-        flexWrap="wrap"
-        alignItems={alignItems}
-        marginLeft={`-${themeGet(`space.${spacing}`, 0)({ spacing })}px`}
-      >
+    <Box marginTop={negativeSpacing} {...rest}>
+      <Box display="flex" flexWrap="wrap" alignItems={alignItems} marginLeft={negativeSpacing}>
         {validChildrenArray.map((child, i) => {
           const spacingProps = { mt: spacing, ml: spacing, mb: 0, mr: 0 };
           if (typeof child === 'string' || child.type === React.Fragment) {
