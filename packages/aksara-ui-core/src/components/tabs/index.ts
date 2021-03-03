90a13cf0fae26395fba3199855c92049cd970492
Rohmat Sasmito <rochmad.26@gmail.com>
2021-03-03 09:45:27 +0700

    fix: missing import around tabs component (#327)

--- packages/aksara-ui-core/src/components/tabs/index.ts
+++ packages/aksara-ui-core/src/components/tabs/index.ts
@@ -1,2 +1,14 @@
 export { default as Tabs } from './components/Tabs';
 export * from './components/Tabs';
+
+export { default as TabList } from './components/TabList';
+export * from './components/TabList';
+
+export { default as Tab } from './components/Tab';
+export * from './components/Tab';
+
+export { default as TabPanels } from './components/TabPanels';
+export * from './components/TabPanels';
+
+export { default as TabPanel } from './components/TabPanel';
+export * from './components/TabPanel';
