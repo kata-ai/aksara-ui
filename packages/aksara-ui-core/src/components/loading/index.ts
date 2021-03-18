c5801c29d7161fff7a81263b76b355617f82fb61
Resi Respati <resir014@gmail.com>
2021-03-18 14:57:06 +0700

    feat(loading): add spinner (#350)
    * feat(loading): add spinner
    
    * fix: add prop docs

--- packages/aksara-ui-core/src/components/loading/index.ts
+++ packages/aksara-ui-core/src/components/loading/index.ts
@@ -1,2 +1,4 @@
 export { default as Circle } from './components/Circle';
 export * from './components/Circle';
+export { default as Spinner } from './components/Spinner';
+export * from './components/Spinner';
