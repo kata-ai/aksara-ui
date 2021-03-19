75a397495ffacef06a95382457fb80621d4fbd50
Rohmad Sasmito <rochmad.26@gmail.com>
2021-03-19 14:03:49 +0700

    style: move border styles to baseStyle

--- packages/aksara-ui-core/src/theme/componentStyles/pill.ts
+++ packages/aksara-ui-core/src/theme/componentStyles/pill.ts
@@ -7,6 +7,9 @@ const pill: ComponentThemeConfig = {
     alignItems: 'center',
     fontSize: 300,
     fontWeight: 400,
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: 'transparent',
   },
   propToScaleMap: [
     ['size', 'sizes'],
@@ -35,17 +38,17 @@ const pill: ComponentThemeConfig = {
       default: {
         backgroundColor: 'green03',
         color: 'grey08',
+        borderColor: 'green03',
       },
       neutral: {
         backgroundColor: 'indigo01',
         color: 'grey08',
-        borderWidth: '1px',
-        borderStyle: 'solid',
         borderColor: 'indigo03',
       },
       info: {
         backgroundColor: 'blue03',
         color: 'grey08',
+        borderColor: 'blue03',
       },
       warning: {
         backgroundColor: 'yellow03',
@@ -54,10 +57,12 @@ const pill: ComponentThemeConfig = {
       critical: {
         backgroundColor: 'red03',
         color: 'grey08',
+        borderColor: 'red03',
       },
       success: {
         backgroundColor: 'green03',
         color: 'grey08',
+        borderColor: 'green03',
       },
     },
     buttonVariants: {
