6d1cb97b0c69ac61e46066e86230fa719b3b4d77
Resi Respati <resir014@gmail.com>
2021-03-22 16:11:43 +0700

    docs: add example for pills

--- packages/aksara-ui-core/src/components/pill/components/Pill.stories.tsx
+++ packages/aksara-ui-core/src/components/pill/components/Pill.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { action } from '@storybook/addon-actions';
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from '@storybook/react';
 import { Box } from '../../../foundations';
 import Pill, { PillProps } from './Pill';
 
