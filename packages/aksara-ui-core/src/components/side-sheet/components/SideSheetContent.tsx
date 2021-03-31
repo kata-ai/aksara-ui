417bb2602a6e7fd6c39b2fbd4d488dd5113224fe
Izzan <nurdinizzan@gmail.com>
2021-03-31 15:53:45 +0700

    feat: new sidesheetcontent

--- packages/aksara-ui-core/src/components/side-sheet/components/SideSheetContent.tsx
+++ packages/aksara-ui-core/src/components/side-sheet/components/SideSheetContent.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
-import { DialogContent, DialogContentProps } from '../../dialog';
+import { Box } from '../../../foundations';
+import { DialogContentProps } from '../../dialog';
 
 export type SideSheetContentProps = DialogContentProps;
 
-const SideSheetContent: React.FC<SideSheetContentProps> = DialogContent;
+const SideSheetContent: React.FC<SideSheetContentProps> = ({ children, ...rest }) => {
+  return (
+    <Box flex="1 1 auto" pt="md" pb={64} px="xl" {...rest}>
+      {children}
+    </Box>
+  )
+};
 
 SideSheetContent.displayName = 'SideSheetContent';
 
