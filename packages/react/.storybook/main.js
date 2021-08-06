568cb0b137dd0e25fe6ac9fd3222359919c8681e
Resi Respati <resir014@gmail.com>
2021-08-06 13:16:28 +0700

    chore: storybook fixes

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -3,10 +3,6 @@ const path = require('path');
 module.exports = {
   stories: ['../src/**/*.{story,stories}.mdx', '../src/**/*.{story,stories}.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-knobs', '@storybook/addon-links'],
-  babel: async options => ({
-    ...options,
-    plugins: [...options.plugins, '@babel/plugin-proposal-class-properties'],
-  }),
   webpackFinal: config => {
     config.resolve.mainFields = ['aksara:src', 'main'];
     return config;
