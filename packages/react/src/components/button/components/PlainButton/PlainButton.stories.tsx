464be87adc6b56527143ca4d674b32a65b25c07f
Resi Respati <resir014@gmail.com>
2021-08-24 01:05:15 +0700

    story(react): plain button stories

--- packages/react/src/components/button/components/PlainButton/PlainButton.stories.tsx
+++ packages/react/src/components/button/components/PlainButton/PlainButton.stories.tsx
@@ -1,5 +1,5 @@
-import { action } from '@storybook/addon-actions';
 import { Story } from '@storybook/react';
+import { IconPen } from '@aksara-ui/icons';
 import * as React from 'react';
 
 import PlainButton, { PlainButtonProps } from './PlainButton';
@@ -23,13 +23,22 @@ export default {
   },
 };
 
-export const Example: Story<PlainButtonProps> = ({ variant, size, isLoading, onClick }) => (
+const Template: Story<PlainButtonProps> = ({ variant, size, isLoading, onClick }) => (
   <PlainButton type="button" variant={variant} size={size} isLoading={isLoading} onClick={onClick}>
     Push Me
   </PlainButton>
 );
+
+export const Example: Story<PlainButtonProps> = Template.bind({});
 Example.args = {
   variant: 'primary',
   size: 'sm',
   isLoading: false,
+};
+
+export const WithIcon: Story<PlainButtonProps> = Template.bind({});
+WithIcon.args = {
+  ...Example.args,
+  icon: IconPen,
+  iconPosition: 'left',
 };
