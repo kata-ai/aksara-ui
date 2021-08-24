d9fc0a8f9dcd03a385cfdffd9ae4c01f860bbaae
Resi Respati <resir014@gmail.com>
2021-08-24 11:14:46 +0700

    test: fixed tests

--- packages/react/src/components/empty-state/components/EmptyState.test.tsx
+++ packages/react/src/components/empty-state/components/EmptyState.test.tsx
@@ -4,16 +4,11 @@ import EmptyState from './EmptyState';
 
 describe('components/Blankslate', () => {
   test('renders with title', () => {
-    const { getByText } = render(<EmptyState variant="page" title="Empty Text" content="Blankslate test" />);
+    const { getByRole } = render(<EmptyState variant="page" title="Empty Text" content="Blankslate test" />);
 
-    const title = getByText('Empty Text');
-    expect(title).toBeInTheDocument();
-  });
-
-  test("doesn't render title in inner variant", () => {
-    const { queryByText } = render(<EmptyState variant="inner" title="Empty Text" content="Blankslate test" />);
-
-    const title = queryByText(/Empty Text/);
-    expect(title).not.toBeInTheDocument();
+    const title = getByRole('heading', {
+      name: /empty text/i,
+    });
+    expect(title).toBeVisible();
   });
 });
