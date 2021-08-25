443da9acec3465430437472416043cb9ac1f3f46
Resi Respati <resir014@gmail.com>
2021-08-25 16:23:51 +0700

    feat(react): secondary nav item

---
+++ packages/react/src/components/navigation/components/SecondaryNavItem.stories.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { IconGrid } from '@aksara-ui/icons';
+import SecondaryNavItem from './SecondaryNavItem';
+
+export default {
+  title: 'Core/Components/Navigation/SecondaryNavItem',
+  component: SecondaryNavItem,
+};
+
+export const Example = () => {
+  return (
+    <SecondaryNavItem
+      title="Flow"
+      icon={IconGrid}
+      href="https://www.youtube.com/watch?v=QjGYHf0jTRA"
+      target="_blank"
+      rel="noopener noreferrer"
+      labelText="Label"
+    />
+  );
+};
+
+export const IsActive = () => {
+  return (
+    <SecondaryNavItem
+      title="Flow"
+      icon={IconGrid}
+      href="https://www.youtube.com/watch?v=QjGYHf0jTRA"
+      target="_blank"
+      rel="noopener noreferrer"
+      labelText="Label"
+      isActive
+    />
+  );
+};
+
+export const Disclosure = () => {
+  return (
+    <SecondaryNavItem
+      title="Flow"
+      icon={IconGrid}
+      href="https://www.youtube.com/watch?v=QjGYHf0jTRA"
+      target="_blank"
+      rel="noopener noreferrer"
+      variant="disclosure"
+    />
+  );
+};
+
+export const Disabled = () => {
+  return <SecondaryNavItem title="Flow" icon={IconGrid} labelText="Label" disabled />;
+};
--- packages/react/src/theme/componentStyles/navigation.ts
+++ packages/react/src/theme/componentStyles/navigation.ts
@@ -48,8 +48,71 @@ const primaryNavItem: ComponentThemeConfig = {
   },
 };
 
+const secondaryNavItem: ComponentThemeConfig = {
+  baseStyle: ({ disabled, isActive }: { theme: DefaultTheme; disabled?: boolean; isActive?: boolean }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    maxWidth: '272px',
+    borderRadius: 12,
+    cursor: disabled ? 'not-allowed' : 'pointer',
+    backgroundColor: disabled ? 'greylight01' : isActive ? 'blue01' : 'transparent',
+    color: disabled ? 'greymed01' : isActive ? 'blue07' : 'greymed04',
+    '&:hover, &:focus': {
+      color: disabled ? 'greymed01' : isActive ? 'blue07' : 'greymed04',
+    },
+    px: 'md',
+    ...(!disabled
+      ? {
+          '&:focus': {
+            boxShadow: `0 0 0 2px rgba(175, 214, 255, 0.7)`,
+          },
+        }
+      : {}),
+    ...(!disabled && !isActive
+      ? {
+          '&:hover': {
+            backgroundColor: 'greylight03',
+          },
+          '&:active': {
+            backgroundColor: 'blue02',
+          },
+        }
+      : {}),
+  }),
+  propToScaleMap: [['variant', 'variants']],
+  scales: {
+    variants: {
+      disclosure: {
+        height: '36px',
+      },
+      default: {
+        height: '40px',
+      },
+    },
+  },
+};
+
+const navItemBadge: ComponentThemeConfig = {
+  baseStyle: ({ disabled, isActive }: { theme: DefaultTheme; disabled?: boolean; isActive?: boolean }) => ({
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 200,
+    fontWeight: 500,
+    px: 'xs',
+    height: '24px',
+    borderRadius: '24px',
+    backgroundColor: isActive ? 'blue02' : 'greylight04',
+    color: disabled ? 'greymed01' : isActive ? 'blue07' : 'greymed04',
+  }),
+};
+
 const navigation = {
   primaryNavItem,
+  secondaryNavItem,
+  navItemBadge,
 };
 
 export default navigation;
