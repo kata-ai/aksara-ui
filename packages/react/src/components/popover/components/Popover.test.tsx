e8aad11dcb7cd5273dbfade503f25fc60ef14438
Resi Respati <resir014@gmail.com>
2021-10-05 10:22:26 +0700

    feat(react): popover 2.0 (#494)
    * feat(react): popover 2.0
    
    * test(react): popover tests
    
    * test(react): popover test fixes
    
    * fix(react): typing errors on tooltip

--- packages/react/src/components/popover/components/Popover.test.tsx
+++ packages/react/src/components/popover/components/Popover.test.tsx
@@ -5,26 +5,26 @@ import Popover from './Popover';
 describe('components/Popover', () => {
   describe('<Popover />', () => {
     test("doesn't render popover content by default", () => {
-      const { queryByText } = render(
+      const { getByRole } = render(
         <Popover trigger={<button type="button">Toggle Popover</button>}>
           <div style={{ width: 250 }}>This is a popover</div>
         </Popover>
       );
 
-      expect(queryByText(/This is a popover/)).not.toBeInTheDocument();
+      expect(getByRole('button', { name: /toggle popover/i })).toBeVisible();
     });
 
     test('is enabled by clicking the trigger', () => {
-      const { getByText, queryByText } = render(
+      const { getByRole, queryByText } = render(
         <Popover trigger={<button type="button">Toggle Popover</button>}>
           <div style={{ width: 250 }}>This is a popover</div>
         </Popover>
       );
 
-      const triggerButton = getByText('Toggle Popover');
+      const triggerButton = getByRole('button', { name: /toggle popover/i });
       fireEvent.click(triggerButton);
 
-      expect(queryByText(/This is a popover/)).toBeInTheDocument();
+      expect(queryByText(/This is a popover/)).toBeVisible();
     });
   });
 });
