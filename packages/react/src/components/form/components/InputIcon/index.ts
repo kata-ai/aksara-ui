72aa86e29d34c25a67bf324bb36dc245bf5dce9c
Resi Respati <resir014@gmail.com>
2021-10-06 15:32:04 +0700

    feat(react): input searchbox (#499)

--- packages/react/src/components/form/components/InputIcon/index.ts
+++ packages/react/src/components/form/components/InputIcon/index.ts
@@ -1,2 +1,5 @@
 export { default as InputIcon } from './InputIcon';
 export * from './InputIcon';
+
+export { default as InputIconButton } from './InputIconButton';
+export * from './InputIconButton';
--- packages/react/src/components/form/components/InputText/InputText.tsx
+++ packages/react/src/components/form/components/InputText/InputText.tsx
@@ -6,17 +6,17 @@ import { useComponentStyles } from '../../../../system';
 export interface InputTextProps
   extends Omit<BoxProps, 'height' | 'width' | 'size'>,
     React.ComponentPropsWithoutRef<'input'> {
-  /** True if this text input has an addon style */
-  addon?: boolean;
   /** True if the input has errors. */
   errors?: boolean;
+  /** True if the input is rounded */
+  rounded?: boolean;
   /** Form size */
   inputSize?: 'md' | 'lg';
 }
 
 const InputText = React.forwardRef<HTMLInputElement, InputTextProps>(
-  ({ className, errors, addon, inputSize = 'md', sx, pl, pr, ...rest }, ref) => {
-    const styles = useComponentStyles('inputText', { size: inputSize, variant: errors ? 'error' : 'default' });
+  ({ className, errors, inputSize = 'md', rounded, sx, pl, pr, ...rest }, ref) => {
+    const styles = useComponentStyles('inputText', { size: inputSize, variant: errors ? 'error' : 'default', rounded });
 
     return (
       <Box
