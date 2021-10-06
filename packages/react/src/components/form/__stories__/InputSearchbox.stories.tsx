a3e9997fe4dae0fb4033e88410594b74b17244f9
Resi Respati <resir014@gmail.com>
2021-10-06 15:15:34 +0700

    feat(react): input searchbox

---
+++ packages/react/src/components/form/__stories__/InputSearchbox.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, Story } from '@storybook/react';
+import * as React from 'react';
+
+import { InputSearchbox, InputSearchboxProps } from '../components/InputSearchbox';
+
+export default {
+  title: 'Core/Components/Form/InputSearchbox',
+  component: InputSearchbox,
+  argTypes: {
+    errors: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    inputSize: {
+      options: ['md', 'lg'],
+    },
+    onClear: {
+      action: 'clicked',
+    },
+  },
+} as Meta<InputSearchboxProps>;
+
+export const Example: Story<InputSearchboxProps> = ({ errors, disabled, inputSize, onClear }) => {
+  return (
+    <InputSearchbox
+      id="textDummy"
+      name="textDummy"
+      placeholder="Search..."
+      autoComplete="off"
+      errors={errors}
+      disabled={disabled}
+      inputSize={inputSize}
+      onClear={onClear}
+    />
+  );
+};
+Example.args = {
+  errors: false,
+  disabled: false,
+  inputSize: 'md',
+};
---
+++ packages/react/src/components/form/components/InputSearchbox/index.ts
@@ -0,0 +1,2 @@
+export { default as InputSearchbox } from './InputSearchbox';
+export * from './InputSearchbox';
