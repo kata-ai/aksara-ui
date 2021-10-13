784b75d2503e26c25d993fb12cae9897df80d497
Resi Respati <resir014@gmail.com>
2021-10-13 16:28:04 +0700

    feat(react): breadcrumb overflow

--- packages/react/src/components/breadcrumb/Breadcrumb.tsx
+++ packages/react/src/components/breadcrumb/Breadcrumb.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Box } from '../../layout';
 import { useComponentStyles } from '../../system';
+import { Popover } from '../popover';
+import BreadcrumbOverflow from './BreadcrumbOverflow';
 
 export interface BreadcrumbProps {
   /** An array of `BreadcrumbItem` components. */
@@ -14,21 +16,59 @@ const Breadcrumb: React.FC<BreadcrumbProps> = ({ items, separator }) => {
   const breadcrumbListStyles = useComponentStyles('breadcrumbList');
   const breadcrumbListItemStyles = useComponentStyles('breadcrumbListItem');
 
+  const [first, rest, last] = React.useMemo(() => {
+    const [first, ...rest] = items;
+    const popped = rest.splice(-2, 2);
+
+    return [first, rest, popped];
+  }, [items]);
+
   return (
     <Box as="nav" aria-label="Breadcrumb" sx={breadcrumbStyles}>
       <Box as="ol" sx={breadcrumbListStyles}>
-        {items.map((item, i) => (
+        <Box as="li" sx={breadcrumbListItemStyles}>
+          <Box>{first}</Box>
+        </Box>
+        {rest.length > 0 && (
+          <Box as="li" sx={breadcrumbListItemStyles}>
+            <Box aria-hidden color="greymed01">
+              {separator ? (
+                React.createElement(separator, { fill: 'currentColor', width: 8, height: 8 })
+              ) : (
+                <span>/</span>
+              )}
+            </Box>
+            <Box>
+              <Popover placement="bottom" trigger={<BreadcrumbOverflow>...</BreadcrumbOverflow>}>
+                <Box p="xs" width="160px">
+                  {rest
+                    .map((item, i) => {
+                      if (React.isValidElement(item)) {
+                        return (
+                          <Box key={`breadcrumb_overflow_${i}`}>
+                            {React.cloneElement(item, { isOverflowItem: true })}
+                          </Box>
+                        );
+                      }
+
+                      return undefined;
+                    })
+                    .filter(Boolean)}
+                </Box>
+              </Popover>
+            </Box>
+          </Box>
+        )}
+        {last.map((item, i) => (
           // eslint-disable-next-line react/no-array-index-key
           <Box as="li" sx={breadcrumbListItemStyles} key={`breadcrumb_${i}`}>
-            {i !== 0 && (
-              <Box aria-hidden color="greymed01">
-                {separator ? (
-                  React.createElement(separator, { fill: 'currentColor', width: 8, height: 8 })
-                ) : (
-                  <span>/</span>
-                )}
-              </Box>
-            )}
+            <Box aria-hidden color="greymed01">
+              {separator ? (
+                React.createElement(separator, { fill: 'currentColor', width: 8, height: 8 })
+              ) : (
+                <span>/</span>
+              )}
+            </Box>
             <Box>{item}</Box>
           </Box>
         ))}
