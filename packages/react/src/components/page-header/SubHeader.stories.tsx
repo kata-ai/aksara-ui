8c6e1cc45171b486dd2ff863884e86a04ca5f502
Mirza <zanuarmirza@gmail.com>
2021-10-15 21:14:12 +0700

    fix: update subheader stories

--- packages/react/src/components/page-header/SubHeader.stories.tsx
+++ packages/react/src/components/page-header/SubHeader.stories.tsx
@@ -1,4 +1,11 @@
+import { IconChevronDown, IconPen } from '@aksara-ui/icons';
+import { action } from '@storybook/addon-actions';
 import * as React from 'react';
+import { Box } from '../../layout/box';
+import { Heading } from '../../typography';
+import { Button, IconButton } from '../button';
+import { ButtonGroup } from '../button-group';
+import { InputSelect } from '../form/components/InputSelect';
 import SubHeader from './SubHeader';
 
 export default {
@@ -7,9 +14,58 @@ export default {
 };
 
 export const StickSubHeader = () => {
-  return <SubHeader label="Title" variant="stick" />;
+  return (
+    <SubHeader
+      label={<Heading scale={400}>Title</Heading>}
+      variant="stick"
+      actions={
+        <ButtonGroup size="md">
+          <Button type="button" icon={IconChevronDown} iconPosition="right" onClick={action('click')}>
+            Label
+          </Button>
+          <Button variant="primary">Label</Button>
+        </ButtonGroup>
+      }
+    />
+  );
 };
 
 export const StandSubHeader = () => {
-  return <SubHeader label="Title" variant="stand" />;
+  return (
+    <SubHeader
+      label={
+        <Box display={['flex']} alignItems="center">
+          <Heading scale={400}>Title</Heading>
+          <IconButton aria-label="Push Me" onClick={action('pen icon clicked')} size="md" type="button" variant="plain">
+            <IconPen aria-hidden fill="currentColor" />
+          </IconButton>
+          <InputSelect
+            width="200px"
+            placeholder="Options"
+            itemToString={item => (item ? `${item.label}` : '')}
+            itemRenderer={item => (
+              <>
+                <Box width={15} mr="sm" />
+                {`${item.label}`}
+              </>
+            )}
+            handleSelectedItemChange={action('selected item change')}
+            items={[
+              { label: 'options 1', value: 'value_1' },
+              { label: 'options 2', value: 'value_2' },
+            ]}
+          />
+        </Box>
+      }
+      variant="stand"
+      actions={
+        <ButtonGroup size="md">
+          <Button type="button" icon={IconChevronDown} iconPosition="right" onClick={action('click')}>
+            Label
+          </Button>
+          <Button variant="primary">Label</Button>
+        </ButtonGroup>
+      }
+    />
+  );
 };
--- packages/react/src/components/page-header/index.ts
+++ packages/react/src/components/page-header/index.ts
@@ -1,2 +1,3 @@
 export { default as PageHeader } from './PageHeader';
+export { default as SubHeader } from './SubHeader';
 export * from './PageHeader';
