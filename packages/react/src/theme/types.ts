989d330bdb49b9acd16c2db95c034d3265e6531c
Mirza <zanuarmirza@gmail.com>
2021-10-22 18:05:31 +0700

    fix: change CSSObject type

--- packages/react/src/theme/types.ts
+++ packages/react/src/theme/types.ts
@@ -1,24 +1,10 @@
-import {
-  CSSPseudoSelectorProps,
-  CSSSelectorObject,
-  EmotionLabel,
-  SystemCssProperties,
-  VariantProperty,
-} from '@styled-system/css';
+import { SystemStyleObject } from '@styled-system/css';
 import { RequiredTheme } from 'styled-system';
 import { AllSystemProps, PseudoSystemProps } from '../system';
 
-export type CSSObject =
-  | AllSystemProps
-  | PseudoSystemProps
-  | SystemCssProperties
-  | CSSPseudoSelectorProps
-  | CSSSelectorObject
-  | VariantProperty
-  | EmotionLabel
-  | Record<string, any>; // fallback handler, type variable css
+export type CSSObject = AllSystemProps | PseudoSystemProps | SystemStyleObject | Record<string, any>; // fallback handler, type variable css
 
-export type ComponentThemeScaleFn<P> = (props: P & { theme: RequiredTheme }) => CSSObject;
+export type ComponentThemeScaleFn<P = Record<string, any>> = (props: P & { theme: RequiredTheme }) => CSSObject;
 
 // FIXME was SystemStyleObject
 export type ComponentThemeScale<P> = CSSObject | ComponentThemeScaleFn<P>;
