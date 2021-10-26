9e13ace196a51206853fa437dea26eb3f50d6c0d
Mirza <zanuarmirza@gmail.com>
2021-10-26 17:17:14 +0700

    fix: use DefaultTheme type for theme value

--- packages/react/src/theme/types.ts
+++ packages/react/src/theme/types.ts
@@ -1,10 +1,10 @@
 import { SystemStyleObject } from '@styled-system/css';
-import { RequiredTheme } from 'styled-system';
+import { DefaultTheme } from 'styled-components';
 import { AllSystemProps, PseudoSystemProps } from '../system/props';
 
 export type CSSObject = AllSystemProps | PseudoSystemProps | SystemStyleObject | Record<string, any>; // fallback handler, type variable css
 
-export type ComponentThemeScaleFn<P = Record<string, any>> = (props: P & { theme: RequiredTheme }) => CSSObject;
+export type ComponentThemeScaleFn<P = Record<string, any>> = (props: P & { theme: DefaultTheme }) => CSSObject;
 
 // FIXME was SystemStyleObject
 export type ComponentThemeScale<P> = CSSObject | ComponentThemeScaleFn<P>;
