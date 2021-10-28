f1ae7113d7722087c08559b0f04e9b0d946ca267
Resi Respati <resir014@gmail.com>
2021-10-28 16:57:23 +0700

    refactor(react): migrate portal to `radix-ui` (#536)

--- packages/react/src/components/dialog/components/Modal.test.tsx
+++ packages/react/src/components/dialog/components/Modal.test.tsx
@@ -36,7 +36,7 @@ describe('components/Modal', () => {
         </Modal>
       );
 
-      const overlay = document.querySelector('[data-aksara-portal] > div');
+      const overlay = document.querySelector('[data-radix-portal] > div');
       if (overlay) {
         fireEvent.click(overlay);
         expect(handleClose).toHaveBeenCalledTimes(1);
@@ -53,7 +53,7 @@ describe('components/Modal', () => {
         </Modal>
       );
 
-      const overlay = document.querySelector('[data-aksara-portal] > div');
+      const overlay = document.querySelector('[data-radix-portal] > div');
       if (overlay) {
         fireEvent.click(overlay);
         expect(handleClose).toHaveBeenCalledTimes(0);
