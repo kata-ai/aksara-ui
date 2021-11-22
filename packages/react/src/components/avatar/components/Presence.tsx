2a86d2b622283256f78a86fcfad555f890e1fa51
M.Mirza Zanuar <zanuarmirza@gmail.com>
2021-11-22 16:53:51 +0700

    Feat/avatar (#584)
    * feat(avatar): add presence option
    
    * feat(avatar): responsive presence

--- packages/react/src/components/avatar/components/Presence.tsx
+++ packages/react/src/components/avatar/components/Presence.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import { Box } from '../../../layout';
-import { useComponentStyles } from '../../../system';
+import { CSSObject, useComponentStyles } from '../../../system';
 
 export interface PresenceProps {
-  label: React.ReactNode;
+  label: string;
   position: 'top' | 'bottom';
   size?: 'sm' | 'md';
 }
 
 const Presence = ({ size, position = 'top', label }: PresenceProps) => {
   const presenceStyle = useComponentStyles('avatarPresence', { size, position });
-
-  return (
-    <Box position="absolute" right="0" sx={presenceStyle}>
-      {label}
-    </Box>
-  );
+  const expandWidth: CSSObject =
+    label.length >= 3
+      ? {
+          paddingRight: '4px',
+          paddingLeft: '4px',
+        }
+      : {};
+  return <Box sx={{ ...presenceStyle, ...expandWidth }}>{label}</Box>;
 };
 export default Presence;
