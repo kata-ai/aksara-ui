7100b831e433af80f74df6f6f2501ae4bf2c0c2d
mirza <zanuarmirza@gmail.com>
2021-11-22 10:07:19 +0700

    feat: dropdown

--- packages/react/src/components/dropdown/components/DropdownMenu/DropdownMenu.tsx
+++ packages/react/src/components/dropdown/components/DropdownMenu/DropdownMenu.tsx
@@ -1,54 +1,37 @@
 import * as React from 'react';
-import { Box, BoxProps } from '../../../../layout';
+import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu';
+import { Box } from '../../../../layout';
 
-export interface DropdownMenuProps extends BoxProps {
-  className?: string;
-  tipOffset?: number;
-  width?: number;
+export interface DropdownMenuProps extends RadixDropdownMenu.DropdownMenuProps {
   children?: React.ReactNode;
+  width?: number;
 }
 
-const MenuArrow: React.FC<Pick<DropdownMenuProps, 'tipOffset'>> = ({ tipOffset }) => {
-  return (
-    <Box
-      sx={{
-        display: 'block',
-        lineHeight: '11px',
-        zIndex: 1,
-        textAlign: 'center',
-        position: 'absolute',
-        top: '-1px',
-      }}
-      style={{ left: tipOffset, textAlign: 'left' }}
-    >
-      <svg width="24" height="12" viewBox="0 0 24 12">
-        <path fill="#fff" fillRule="evenodd" d="M20 12l-8-8-12 12" />
-      </svg>
-    </Box>
-  );
-};
+// const MenuArrow: React.FC<Pick<DropdownMenuProps, 'tipOffset'>> = ({ tipOffset }) => {
+//   return (
+//     <Box
+//       sx={{
+//         display: 'block',
+//         lineHeight: '11px',
+//         zIndex: 1,
+//         textAlign: 'center',
+//         position: 'absolute',
+//         top: '-1px',
+//       }}
+//       style={{ left: tipOffset, textAlign: 'left' }}
+//     >
+//       <svg width="24" height="12" viewBox="0 0 24 12">
+//         <path fill="#fff" fillRule="evenodd" d="M20 12l-8-8-12 12" />
+//       </svg>
+//     </Box>
+//   );
+// };
 
-const DropdownMenu: React.ForwardRefRenderFunction<HTMLDivElement, DropdownMenuProps> = (
-  { tipOffset, width, children, ...rest },
-  ref
-) => {
+const DropdownMenu: React.ForwardRefRenderFunction<HTMLDivElement, DropdownMenuProps> = ({ children, width }) => {
   return (
-    <Box ref={ref} display="inline-block" position="relative" {...rest}>
-      {tipOffset && <MenuArrow tipOffset={tipOffset} />}
-      <Box
-        display="inline-block"
-        textAlign="left"
-        width={width}
-        maxWidth="100vw"
-        marginTop={11}
-        borderRadius="xs"
-        overflow="hidden"
-        boxShadow={3}
-        py="xs"
-      >
-        {children}
-      </Box>
-    </Box>
+    <RadixDropdownMenu.Root>
+      <Box width={width}>{children}</Box>
+    </RadixDropdownMenu.Root>
   );
 };
 
