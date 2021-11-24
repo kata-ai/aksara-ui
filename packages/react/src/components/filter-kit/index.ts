cef4971267be5ccddd3450e63330f73db13f7b2a
mirza <zanuarmirza@gmail.com>
2021-11-24 17:16:27 +0700

    feat(react): filter-kit and page-header

---
+++ packages/react/src/components/filter-kit/index.ts
@@ -0,0 +1,2 @@
+export { default as FilterPageHeader } from './FilterKit';
+export * from './FilterKit';
--- packages/react/src/components/page-header/index.ts
+++ packages/react/src/components/page-header/index.ts
@@ -2,4 +2,3 @@ export { default as PageHeader } from './PageHeader';
 export { default as SubHeader } from './SubHeader';
 export * from './PageHeader';
 export * from './SubHeader';
-export * from './components';
