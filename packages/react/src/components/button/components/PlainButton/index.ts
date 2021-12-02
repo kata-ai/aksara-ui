a02dee7f6c640e1aa464ce599f56bfa895800d14
M.Mirza Zanuar <zanuarmirza@gmail.com>
2021-12-02 14:25:08 +0700

    feat(react): add plain button anchor (#616)

--- packages/react/src/components/button/components/PlainButton/index.ts
+++ packages/react/src/components/button/components/PlainButton/index.ts
@@ -1,4 +1,6 @@
 export { default as PlainButton } from './PlainButton';
+export { default as PlainButtonAnchor } from './PlainButtonAnchor';
 export * from './PlainButton';
+export * from './PlainButtonAnchor';
 
 export * from './types';
