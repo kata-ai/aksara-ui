ea993d54f9f0abc610759675ce90d8ee00bb4cb5
Mirza <zanuarmirza@gmail.com>
2021-12-10 09:05:33 +0700

    feat(list): wip

--- packages/react/src/components/listResource/components/List.tsx
+++ packages/react/src/components/listResource/components/List.tsx
@@ -8,10 +8,29 @@ import { Box } from '../../../layout';
  * renderItem props, to render per item
  *
  */
+/**
+ * api
+ * selectedItems to show which item is selected
+ * OR
+ * handling selectedItem in items data
+ *   note: this causing item data mixed with state item
+ *        not recommended
+ * conclusion: use selectedItems to show selected items
+ */
 
+/**
+ * api
+ * onSelectionChange to get items selected changes
+ * as array
+ * OR
+ * as item
+ * onSelectionChange will be used to change selectedItems data
+ */
 export interface ListProp<T> {
   items: T[];
   renderItem: (props: T) => React.ReactElement;
+  selectedItems: T[];
+  onSelectionChange: (change: T) => void;
 }
 
 const List = <T,>({ items, renderItem }: ListProp<T>) => {
