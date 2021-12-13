ddaafba60b16323700953c991be4f33758040ba3
mirza <zanuarmirza@gmail.com>
2021-12-13 13:56:15 +0700

    feat(listResource): add list stories with selected item

--- packages/react/src/components/listResource/components/List.stories.tsx
+++ packages/react/src/components/listResource/components/List.stories.tsx
@@ -8,6 +8,7 @@ export default {
   component: List,
 };
 interface Item {
+  id: string;
   name: string;
   status: string;
   role: string;
@@ -17,6 +18,7 @@ export const Example = () => {
     {
       selected: true,
       data: {
+        id: '1',
         name: 'Name Dummy 1',
         status: 'approved',
         role: 'Manager',
@@ -25,6 +27,7 @@ export const Example = () => {
     {
       selected: false,
       data: {
+        id: '2',
         name: 'Name Dummy 2',
         status: 'Pending',
         role: 'Techinal Support',
@@ -69,6 +72,94 @@ export const Example = () => {
         header={renderHeader()}
         renderItem={renderListItem}
         onSelectItem={data => console.log('onSelectItem', data)}
+      />
+    </Box>
+  );
+};
+
+// TODO
+/**
+ * create stories for case header with selected item
+ *
+ * HOW
+ * create state to handle state selection
+ * item list raw (all data)
+ * selected item
+ * item list with select value (using map) -> need to add isActive behavior -> time complexity O(n)
+ * untuk create data list + selected -> complexity 0 n log n
+ * ubah semua selectedItem ke maphash
+ * get value selected dari maphashnya
+ *
+ */
+
+export const WithSelectedItem = () => {
+  const listItem = [
+    {
+      id: '1',
+      name: 'Name Dummy 1',
+      status: 'approved',
+      role: 'Manager',
+    },
+    {
+      id: '2',
+      name: 'Name Dummy 2',
+      status: 'Pending',
+      role: 'Techinal Support',
+    },
+  ];
+  const [selectedItem, setSelectedItem] = React.useState<Record<string, boolean>>({});
+  // n
+  const listItemTable = () => {
+    return listItem.map(item => {
+      return {
+        selected: selectedItem ? selectedItem[item.id] : false,
+        data: item,
+      };
+    });
+  };
+  const renderListItem = (data: Item) => (
+    <Box display="flex" width="100%">
+      <Box flex="1">
+        <Text>{data.name}</Text>
+      </Box>
+      <Box width="200px">
+        <Text>{data.role}</Text>
+      </Box>
+      <Box width="200px">
+        <Text>{data.status}</Text>
+      </Box>
+    </Box>
+  );
+  const renderHeader = () => {
+    return (
+      <Box display="flex" width="100%">
+        <Box flex="1">
+          <Text fontWeight="700" fontSize="12px" lineHeight="16px">
+            Name
+          </Text>
+        </Box>
+        <Box width="200px" fontWeight="700" fontSize="12px" lineHeight="16px">
+          <Text>Role</Text>
+        </Box>
+        <Box width="200px" fontWeight="700" fontSize="12px" lineHeight="16px">
+          <Text>Status</Text>
+        </Box>
+      </Box>
+    );
+  };
+  return (
+    <Box>
+      <List
+        items={listItemTable()}
+        keyExtractor={(data, index) => `${data.name}-${index}`}
+        header={renderHeader()}
+        renderItem={renderListItem}
+        onSelectItem={data =>
+          setSelectedItem(prev => ({
+            ...prev,
+            [data.id]: prev[data.id] ? !prev[data.id] : true,
+          }))
+        }
       />
     </Box>
   );
