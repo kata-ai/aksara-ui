979cd4f19f90302e65f14aaca37f9adfd2d25681
mirza <zanuarmirza@gmail.com>
2021-12-14 17:43:47 +0700

    fix: add icon at topbar and savebar

--- packages/react/src/components/saveBar/components/SaveBar.tsx
+++ packages/react/src/components/saveBar/components/SaveBar.tsx
@@ -6,15 +6,20 @@ import { useComponentStyles } from '../../../system';
 export interface SaveBarProps extends BoxProps {
   /** set label of savebar  */
   label: string;
+  /** set icon of savebar  */
+  icon?: React.ReactNode;
   /** render actions */
   actions: React.ReactNode;
   /** Set max width of content */
   maxWidth: string | number;
 }
-const SaveBar: React.FC<SaveBarProps> = ({ label, actions, maxWidth, sx, ...rest }) => {
+const SaveBar: React.FC<SaveBarProps> = ({ label, icon, actions, maxWidth, sx, ...rest }) => {
   const savebarStyle = useComponentStyles('saveBarBase');
   return (
     <Box sx={{ ...savebarStyle, ...sx }} {...rest}>
+      <Box position="absolute" left="24px">
+        {icon}
+      </Box>
       <Box display="flex" alignItems="center" justifyContent="space-between" width="100%" maxWidth={maxWidth}>
         <Text color="greylight01" scale={400}>
           {label}
