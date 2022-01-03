bea60308f26b2de574c5ed8ba69ca66ae827fe65
mirza <zanuarmirza@gmail.com>
2022-01-03 19:00:05 +0700

    feat: scroll area

---
+++ packages/react/src/components/scroll-area/components/ScrollArea.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area';
+import { Box } from '../../../layout';
+import { useComponentStyles } from '../../../system';
+
+export interface ScrollAreaProps extends ScrollAreaPrimitive.ScrollAreaProps {
+  children: React.ReactNode;
+  scroll: 'horizontal' | 'vertical' | 'all';
+  cornerStyle?: React.CSSProperties;
+  style?: React.CSSProperties;
+  width?: number | string;
+  height?: number | string;
+}
+
+export const ScrollArea: React.FC<ScrollAreaProps> = ({
+  children,
+  height,
+  width,
+  scroll,
+  cornerStyle,
+  style,
+  ...rest
+}) => {
+  const scrollBarStyle = useComponentStyles('scrollBar');
+  const scrollBarThumbsStyle = useComponentStyles('scrollBarThumbs');
+  const renderScrollBar = () => {
+    if (scroll === 'all') {
+      return (
+        <>
+          <ScrollAreaPrimitive.Scrollbar asChild orientation="horizontal">
+            <Box sx={scrollBarStyle}>
+              <ScrollAreaPrimitive.ScrollAreaThumb asChild>
+                <Box sx={scrollBarThumbsStyle} />
+              </ScrollAreaPrimitive.ScrollAreaThumb>
+            </Box>
+          </ScrollAreaPrimitive.Scrollbar>
+          <ScrollAreaPrimitive.Scrollbar asChild orientation="vertical">
+            <Box sx={scrollBarStyle}>
+              <ScrollAreaPrimitive.ScrollAreaThumb asChild>
+                <Box sx={scrollBarThumbsStyle} />
+              </ScrollAreaPrimitive.ScrollAreaThumb>
+            </Box>
+          </ScrollAreaPrimitive.Scrollbar>
+        </>
+      );
+    }
+    return (
+      <ScrollAreaPrimitive.Scrollbar asChild orientation={scroll}>
+        <Box sx={scrollBarStyle}>
+          <ScrollAreaPrimitive.ScrollAreaThumb asChild>
+            <Box sx={scrollBarThumbsStyle} />
+          </ScrollAreaPrimitive.ScrollAreaThumb>
+        </Box>
+      </ScrollAreaPrimitive.Scrollbar>
+    );
+  };
+  return (
+    <ScrollAreaPrimitive.Root asChild {...rest}>
+      <Box style={style} height={height} width={width} overflow={'hidden'}>
+        <ScrollAreaPrimitive.Viewport>
+          <Box width={width} height={height}>
+            {children}
+          </Box>
+        </ScrollAreaPrimitive.Viewport>
+        {renderScrollBar()}
+        {cornerStyle && <ScrollAreaPrimitive.ScrollAreaCorner style={cornerStyle} />}
+      </Box>
+    </ScrollAreaPrimitive.Root>
+  );
+};
