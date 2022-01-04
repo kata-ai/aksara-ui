8910d0b4f4036fe6cb1244b71bf5b668382e7a46
mirza <zanuarmirza@gmail.com>
2022-01-04 12:01:55 +0700

    style(side-sheet): update style side-sheet seperator

--- packages/react/src/components/side-sheet/components/SideSheetContent.tsx
+++ packages/react/src/components/side-sheet/components/SideSheetContent.tsx
@@ -5,7 +5,7 @@ export type SideSheetContentProps = BoxProps;
 
 const SideSheetContent: React.FC<SideSheetContentProps> = ({ children, ...rest }) => {
   return (
-    <Box flex="1 1 auto" py="md" px="lg" {...rest}>
+    <Box flex="1 1 auto" {...rest}>
       {children}
     </Box>
   );
