b3553414d96a89e794b9d26000978e456f3e6b67
M.Mirza Zanuar <zanuarmirza@gmail.com>
2022-01-04 15:03:36 +0700

    style(scroll-area): fix accordion style (#687)
    * style(scroll-area): fix accordion style
    
    * test(accordion): add accordion test
    
    * docs(accordion): add readme accordion

--- packages/react/src/components/accordion/components/Accordion_Legacy/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Accordion } from './Accordion';
-export { default as AccordionButton } from './AccordionButton';
-export { default as AccordionItem } from './AccordionItem';
-export { default as AccordionPanel } from './AccordionPanel';
