312a475f1e00f02e5b8257f9dc7a0dac1013b9ad
M.Mirza Zanuar <zanuarmirza@gmail.com>
2022-01-19 15:20:15 +0700

    feat: savebar fade-in fade-out transition (#732)

--- packages/react/src/components/save-bar/components/SaveBar.tsx
+++ packages/react/src/components/save-bar/components/SaveBar.tsx
@@ -1,9 +1,49 @@
 import * as React from 'react';
+import { Transition } from 'react-transition-group';
+import styled, { keyframes } from 'styled-components';
 import { Box, BoxProps } from '../../../layout';
 import { Text } from '../../../typography';
 import { useComponentStyles } from '../../../system';
 import { Portal } from '../../../helpers';
 
+const EnterAnimation = keyframes`
+  0% {
+    opacity: 0;
+    transform: translate(0, -25%);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const LeaveAnimation = keyframes`
+  0% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  100% {
+    opacity: 0;
+    transform: translate(0, -25%);
+  }
+`;
+
+const SaveBarContainer = styled(Box)`
+  &[data-state='entering'],
+  &[data-state='entered'] {
+    animation-fill-mode: forwards;
+    animation-name: ${EnterAnimation};
+    animation-duration: 300ms;
+  }
+
+  &[data-state='exiting'] {
+    animation-fill-mode: forwards;
+    animation-name: ${LeaveAnimation};
+    animation-duration: 300ms;
+  }
+`;
 export interface SaveBarProps extends BoxProps {
   /** set label of savebar  */
   label: string;
@@ -16,23 +56,36 @@ export interface SaveBarProps extends BoxProps {
   /**  */
   isShow: boolean;
 }
+
 const SaveBar: React.FC<SaveBarProps> = ({ label, icon, actions, maxWidth, sx, isShow, ...rest }) => {
   const savebarStyle = useComponentStyles('saveBarBase');
   return (
     <Portal>
-      {isShow && (
-        <Box sx={{ ...savebarStyle, ...sx }} {...rest}>
-          <Box position="absolute" left="24px">
-            {icon}
-          </Box>
-          <Box display="flex" alignItems="center" justifyContent="space-between" width="100%" maxWidth={maxWidth}>
-            <Text color="greylight01" scale={400}>
-              {label}
-            </Text>
-            <Box>{actions}</Box>
-          </Box>
-        </Box>
-      )}
+      <Transition
+        appear
+        in={isShow}
+        timeout={{
+          enter: 300,
+          exit: 300,
+        }}
+        unmountOnExit
+      >
+        {state => {
+          return (
+            <SaveBarContainer data-state={state} sx={{ ...savebarStyle, ...sx }} {...rest}>
+              <Box position="absolute" left="24px">
+                {icon}
+              </Box>
+              <Box display="flex" alignItems="center" justifyContent="space-between" width="100%" maxWidth={maxWidth}>
+                <Text color="greylight01" scale={400}>
+                  {label}
+                </Text>
+                <Box>{actions}</Box>
+              </Box>
+            </SaveBarContainer>
+          );
+        }}
+      </Transition>
     </Portal>
   );
 };
