41df7a799dee5556b29e7a077faf0b67d616f1f1
M.Mirza Zanuar <zanuarmirza@gmail.com>
2022-02-10 08:43:41 +0700

    fix: input select tag add & remove tag handler (#781)

--- packages/react/src/layout/wrap/components/WrapItem.tsx
+++ packages/react/src/layout/wrap/components/WrapItem.tsx
@@ -5,7 +5,18 @@ export type WrapItemProps = BoxProps & React.LiHTMLAttributes<HTMLLIElement>;
 
 const WrapItem = React.forwardRef<HTMLLIElement, WrapItemProps>(({ children, ...rest }, ref) => {
   return (
-    <Box as="li" ref={ref} {...rest}>
+    <Box
+      as="li"
+      ref={ref}
+      sx={{
+        borderRadius: 'lg',
+        '&:focus-visible': {
+          outline: 'none',
+          boxShadow: `0 0 0 2px rgba(175, 214, 255, 0.7)`,
+        },
+      }}
+      {...rest}
+    >
       {children}
     </Box>
   );
